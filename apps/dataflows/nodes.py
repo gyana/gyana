19c9525f3b6b867459b9494a1d7608ba37383d25
saschahofmann <sascha.hofmann@gyana.co.uk>
2021-05-05 09:35:46 +0200

    add to development.md

--- apps/dataflows/nodes.py
+++ apps/dataflows/nodes.py
@@ -1,26 +1,4 @@
-from abc import ABC, abstractmethod
-
-from apps.datasets.models import Dataset
 from lib.bigquery import ibis_client
-
-
-class Node(ABC):
-    def __init__(self, node):
-        self.node = node
-
-    @abstractmethod
-    def get_query(self):
-        pass
-
-
-class Input(Node):
-    def __init__(self, dataset):
-        self.dataset = dataset
-
-    def get_query(self):
-        dataset = Dataset.objects.get(pk=int(self.dataset))
-        conn = ibis_client()
-        return conn.table(dataset.table_id)
 
 
 def get_input_query(node):
