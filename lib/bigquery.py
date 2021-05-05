283da2043e45e581d4511d56fcb18bb7d73d865d
David Das Kell <davidambrosekell@gmail.com>
2021-05-05 04:59:55 +0100

    Basic caching strategy

--- lib/bigquery.py
+++ lib/bigquery.py
@@ -39,9 +39,10 @@ def create_external_table(dataset: Dataset):
     client = bigquery_client()
 
     bq_dataset = client.get_dataset(DATASET_ID)
+    external_table_id = f"table_{dataset.id}_external"
     table_id = f"table_{dataset.id}"
 
-    table = bigquery.Table(bq_dataset.table(table_id))
+    external_table = bigquery.Table(bq_dataset.table(external_table_id))
 
     if dataset.kind == Dataset.Kind.GOOGLE_SHEETS:
         # https://cloud.google.com/bigquery/external-data-drive#python
@@ -55,8 +56,12 @@ def create_external_table(dataset: Dataset):
 
     external_config.autodetect = True
 
-    table.external_data_configuration = external_config
-    table = client.create_table(table, exists_ok=True)
+    external_table.external_data_configuration = external_config
+    external_table = client.create_table(external_table, exists_ok=True)
+
+    client.query(
+        f"CREATE OR REPLACE TABLE {DATASET_ID}.{table_id} AS SELECT * FROM {DATASET_ID}.{external_table_id}"
+    )
 
     dataset.table_id = table_id
     dataset.save()
