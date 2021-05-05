c2f61a1dbc3b23b50c5a2e840d272dc3e5d6d349
David Das Kell <davidambrosekell@gmail.com>
2021-05-05 01:04:11 +0100

    Project foreign key and turbo-frames on one page

--- apps/dashboards/models.py
+++ apps/dashboards/models.py
@@ -1,13 +1,15 @@
+from apps.projects.models import Project
 from django.db import models
 
 
 class Dashboard(models.Model):
     name = models.CharField(max_length=255)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True, editable=False)
     updated = models.DateTimeField(auto_now=True, editable=False)
 
     class Meta:
-        ordering = ('-created', )
+        ordering = ("-created",)
 
     def __str__(self):
         return self.name
