c6cf2d6c08aca6a82e58b870771bd43fb7cf4eba
David Das Kell <davidambrosekell@gmail.com>
2021-05-14 01:54:15 +0100

    Remove lib/bigquery entirely

--- apps/widgets/bigquery.py
+++ apps/widgets/bigquery.py
@@ -1,43 +1,6 @@
-from datetime import datetime
-
 from apps.filters.models import Filter
-from apps.tables.models import Table
 from apps.widgets.models import Widget
-from apps.workflows.models import Node, Workflow
-
-from lib.clients import DATAFLOW_ID, bigquery_client, ibis_client
-
-DEFAULT_LIMIT = 10
-
-
-def run_workflow(workflow: Workflow):
-    output_nodes = workflow.node_set.filter(kind=Node.Kind.OUTPUT).all()
-
-    for node in output_nodes:
-        client = bigquery_client()
-        query = node.get_query().compile()
-
-        try:
-            client.query(
-                f"CREATE OR REPLACE TABLE {DATAFLOW_ID}.{node.table.bq_table} as ({query})"
-            ).result()
-
-        except Table.DoesNotExist:
-            table_id = f"table_{node.pk}"
-            client.query(
-                f"CREATE OR REPLACE TABLE {DATAFLOW_ID}.{table_id} as ({query})"
-            ).result()
-
-            table = Table(
-                source=Table.Source.WORKFLOW_NODE,
-                bq_table=table_id,
-                bq_dataset=DATAFLOW_ID,
-                project=workflow.project,
-                workflow_node=node,
-            )
-            table.save()
-
-    workflow.last_run = datetime.now()
+from lib.clients import ibis_client
 
 
 def query_widget(widget: Widget):
