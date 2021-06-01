478a107e93a246d8f17d7bc7cd23397ed5353705
David Das Kell <davidambrosekell@gmail.com>
2021-06-01 23:01:04 +0100

    Refactor and add more documentation

--- apps/widgets/forms.py
+++ apps/widgets/forms.py
@@ -1,40 +1,11 @@
 from apps.tables.models import Table
+from apps.utils.live_form import LiveUpdateForm
 from django import forms
-from django.forms.widgets import HiddenInput
 
 from .models import Widget
 
 
-class LiveForm(forms.ModelForm):
-    live = forms.CharField(widget=forms.HiddenInput(), required=True)
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        live_fields = self.get_live_fields()
-
-        if self.is_live:
-            live_fields += ["live"]
-
-        self.fields = {k: v for k, v in self.fields.items() if k in live_fields}
-
-    def is_live(self):
-        return "submit" in self.data
-
-    def get_live_field(self, name):
-
-        # data populated by POST request in update
-        if name in self.data:
-            return self.data[name]
-
-        # data populated from database
-        return self.initial[name]
-
-    def get_live_fields(self):
-        raise NotImplementedError()
-
-
-class WidgetConfigForm(LiveForm):
+class WidgetConfigForm(LiveUpdateForm):
 
     label = forms.ChoiceField(choices=())
     value = forms.ChoiceField(choices=())
