efcf547736eee319ee1ef2d40e7c42cefdc12d93
David Das Kell <davidambrosekell@gmail.com>
2021-06-01 22:54:18 +0100

    Add comments

--- assets/javascript/controllers/live_update_controller.js
+++ assets/javascript/controllers/live_update_controller.js
@@ -3,7 +3,7 @@ import morphdom from 'morphdom'
 
 export default class extends Controller {
   listener = async () => {
-    // requestSubmit required for turbo-frame
+    // manually POST the form and get HTML response
     const data = new FormData(this.element)
     const result = await fetch(this.element.action, {
       method: 'POST',
@@ -11,32 +11,14 @@ export default class extends Controller {
     })
     const text = await result.text()
 
+    // Extract the form element and morph into the DOM
     const parser = new DOMParser()
     const doc = parser.parseFromString(text, 'text/html')
-
     const newForm = doc.querySelector('form')
-
     morphdom(this.element, newForm)
   }
 
   connect() {
     this.element.addEventListener('change', this.listener)
-  }
-
-  update(event) {
-    this.element.removeEventListener('change', this.listener)
-
-    // provide information to server on clicked button
-    const hidden_input = document.createElement('input')
-    hidden_input.setAttribute('type', 'hidden')
-    hidden_input.setAttribute('name', event.target.getAttribute('data-name'))
-    this.element.appendChild(hidden_input)
-
-    this.element.setAttribute('data-turbo-frame', '_top')
-
-    event.preventDefault()
-    event.target.setAttribute('type', 'submit')
-
-    this.element.requestSubmit()
   }
 }
