3cbfa7f5c959ffa41f300d6435dc712f35eaa8a8
saschahofmann <sascha.hofmann@gyana.co.uk>
2021-06-02 11:24:17 +0200

    styling

--- templates/javascript/SourceSelect.tsx
+++ templates/javascript/SourceSelect.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react'
-import { Listbox } from '@headlessui/react'
+import React, { Fragment, useState } from 'react'
+import { Listbox, Transition } from '@headlessui/react'
 import ReactDOM from 'react-dom'
 
 const SourceSelect_: React.FC<{ options; selected: number }> = ({ options, selected }) => {
@@ -7,14 +7,50 @@ const SourceSelect_: React.FC<{ options; selected: number }> = ({ options, selec
 
   return (
     <Listbox value={option} onChange={setOption}>
-      <Listbox.Button>{option.label}</Listbox.Button>
-      <Listbox.Options>
-        {options.map((o) => (
-          <Listbox.Option key={o.id} value={o}>
-            {o.label}
-          </Listbox.Option>
-        ))}
-      </Listbox.Options>
+      <Listbox.Button className='relative w-full py-2 pl-3 pr-10 text-left bg-white rounded-lg border border-gray'>
+        <span className='block truncate'>{option.label}</span>
+        <span className='absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none'>
+          <i className='w-5 h-5 text-gray fa fa-chevron-down' />
+        </span>
+      </Listbox.Button>
+      <Transition
+        as={Fragment}
+        leave='transition ease-in duration-100'
+        leaveFrom='opacity-100'
+        leaveTo='opacity-0'
+      >
+        <Listbox.Options className='absolute w-full py-1 mt-1 overflow-auto text-base bg-white rounded-md max-h-60 focus:outline-none border border-gray'>
+          {options.map((o) => (
+            <Listbox.Option
+              key={o.id}
+              value={o}
+              className={({ active }) =>
+                `${active ? 'text-orange bg-orange-20' : 'text-black-50'}
+                cursor-default select-none relative py-2 pl-8 pr-4 flex flex-row items-center`
+              }
+            >
+              {({ selected, active }) => (
+                <>
+                  <i
+                    className={`${o.type === 'integration' ? 'far fa-link' : 'far fa-stream'} mr-3`}
+                  />
+                  <span className={`${selected ? 'font-medium' : 'font-normal'} block truncate`}>
+                    {o.label}
+                  </span>
+                  {selected ? (
+                    <span
+                      className={`${active ? 'text-orange' : 'text-orange-50'}
+                            absolute inset-y-0 right-0 flex items-center pr-3`}
+                    >
+                      <i className='fad fa-check w-5 h-5' />
+                    </span>
+                  ) : null}
+                </>
+              )}
+            </Listbox.Option>
+          ))}
+        </Listbox.Options>
+      </Transition>
       <input type='hidden' name='input_table' value={option.id} />
     </Listbox>
   )
@@ -23,6 +59,8 @@ const SourceSelect_: React.FC<{ options; selected: number }> = ({ options, selec
 class SourceSelect extends HTMLElement {
   connectedCallback() {
     const mountPoint = document.createElement('div')
+    // Because the Select dropdown will be absolute positioned we need to make the outer div relative
+    mountPoint.setAttribute('class', 'relative')
 
     const options = JSON.parse(this.attributes['options'].value)
     const selected = parseInt(this.attributes['selected'].value)
