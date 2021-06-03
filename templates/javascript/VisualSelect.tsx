d1295ba45c9a2dbf7e7e98f423b6a90df64cc21b
saschahofmann <sascha.hofmann@gyana.co.uk>
2021-06-03 15:03:52 +0200

    add default widget kind

--- templates/javascript/VisualSelect.tsx
+++ templates/javascript/VisualSelect.tsx
@@ -21,7 +21,7 @@ const VisualSelect_: React.FC<{ selected: Kind }> = ({ selected }) => {
 
   return (
     <Listbox value={kind} onChange={setKind}>
-      <Listbox.Button className='relative w-full py-2 pl-3 pr-10 text-left text-lg bg-white rounded-lg border border-gray focus:outline-none'>
+      <Listbox.Button className='relative w-full text-2xl py-4 pl-8 pr-10 text-left bg-white rounded-lg border border-gray focus:outline-none'>
         <span className='block truncate'>{kind.name}</span>
         <span className='absolute inset-y-0 right-0 flex items-center pr-4 pointer-events-none'>
           <i className='text-gray fa fa-chevron-down' />
