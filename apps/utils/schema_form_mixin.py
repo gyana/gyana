f527399e092c416e73511f5f8fdc125705865db5
David Das Kell <davidambrosekell@gmail.com>
2021-06-03 02:37:56 +0100

    Fix

--- apps/utils/schema_form_mixin.py
+++ apps/utils/schema_form_mixin.py
@@ -2,11 +2,11 @@ class SchemaFormMixin:
     @property
     def column_type(self):
         column = self.get_live_field("column")
-        if column in self.schema:
+        if self.schema and column in self.schema:
             return self.schema[column].name
         return None
 
     def __init__(self, *args, **kwargs):
-        self.schema = kwargs.pop("schema")
+        self.schema = kwargs.pop("schema", None)
 
         super().__init__(*args, **kwargs)
