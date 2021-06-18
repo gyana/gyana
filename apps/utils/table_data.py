5109c0f915ef63732c0d02cf6afbab61077b30d3
saschahofmann <sascha.hofmann@gyana.co.uk>
2021-06-18 16:37:04 +0100

    stage table preview

--- apps/utils/table_data.py
+++ apps/utils/table_data.py
@@ -1,7 +1,7 @@
 from django_tables2 import Column, Table
 from django_tables2.data import TableData
 from django_tables2.templatetags.django_tables2 import QuerystringNode
-from lib.clients import ibis_client
+from lib.clients import bigquery_client, ibis_client
 
 # Monkey path the querystring templatetag for the pagination links
 # Without this links only lead to the whole document url and add query parameter
@@ -33,18 +33,25 @@ class BigQueryTableData(TableData):
         data,
     ):
         self.data = data
+        self.client = bigquery_client()
 
     def __getitem__(self, page: slice):
         """Fetches the data for the current page"""
-        conn = ibis_client()
-        df = conn.execute(self.data.limit(page.stop - page.start, offset=page.start))
+        df = (
+            self.client.query(
+                self.data.limit(page.stop - page.start, offset=page.start).compile()
+            )
+            .result()
+            .to_dataframe(create_bqstorage_client=False)
+        )
         return df.to_dict(orient="records")
 
     # TODO: This request slows down the loading of data a lot.
     def __len__(self):
         """Fetches the total size from BigQuery"""
-        conn = ibis_client()
-        return conn.execute(self.data.count())
+        return self.client.query(self.data.count().compile()).to_dataframe(
+            create_bqstorage_client=False
+        )
 
     # Not sure when or whether this is used at the moment
     def __iter__(self):
