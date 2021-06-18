fe64de50d514f1978f9d7e915697055d7ef26615
saschahofmann <sascha.hofmann@gyana.co.uk>
2021-06-18 17:16:59 +0100

    make a little speedup

--- apps/filters/views.py
+++ apps/filters/views.py
@@ -1,6 +1,7 @@
 import ibis.expr.datatypes as dt
 from apps.widgets.models import Widget
 from apps.workflows.models import Node
+from lib.clients import get_dataframe
 from rest_framework.decorators import api_view
 from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
@@ -28,14 +29,16 @@ def autocomplete_options(request):
     query = parent.get_query()
 
     options = (
-        query[query[column].cast(dt.String()).lower().startswith(q)]
-        .projection([column])
-        .distinct()
-        # For now simply limit the results to 20
-        # Later we could also make the list scrollable
-        # and send in chunks
-        .limit(20)
-        .execute()
+        get_dataframe(
+            query[query[column].cast(dt.String()).lower().startswith(q)]
+            .projection([column])
+            .distinct()
+            # For now simply limit the results to 20
+            # Later we could also make the list scrollable
+            # and send in chunks
+            .limit(20)
+            .compile()
+        )
         .values.flatten()
         .tolist()
     )
