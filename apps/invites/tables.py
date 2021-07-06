7c73065faf98075b520e2d173727b9c1d099a31d
David Das Kell <davidambrosekell@gmail.com>
2021-07-06 15:40:37 +0100

    Delete invite

--- apps/invites/tables.py
+++ apps/invites/tables.py
@@ -1,4 +1,5 @@
 import django_tables2 as tables
+from django_tables2.utils import A
 
 from .models import Invite
 
@@ -12,3 +13,6 @@ class InviteTable(tables.Table):
     email = tables.Column(linkify=True)
 
     resend = tables.TemplateColumn(template_name="invites/resend.html")
+    delete = tables.LinkColumn(
+        viewname="team_invites:delete", args=(A("team__id"), A("id")), text="Delete"
+    )
