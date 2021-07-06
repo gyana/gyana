28921801e294cc70c9f4082fb4617ed0d5844fe6
David Das Kell <davidambrosekell@gmail.com>
2021-07-06 13:08:48 +0100

    It works

--- apps/invites/signals.py
+++ apps/invites/signals.py
@@ -1,21 +1,30 @@
 from allauth.account.signals import user_signed_up
 from django.dispatch import receiver
 
-from .invitations import get_invitation_id_from_request, process_invitation
+from apps.users.models import CustomUser
+from invitations.signals import invite_accepted
+
 from .models import Invite
 
 
+def add_user_to_accepted_teams(user: CustomUser):
+
+    teams = user.teams.all()
+    for invite in Invite.objects.filter(email=user.email, accepted=True).all():
+        if not invite.team in teams:
+            user.teams.add(invite.team)
+
+    user.save()
+
+
+@receiver(invite_accepted)
+def add_existing_user_to_team(email, **kwargs):
+    user = CustomUser.objects.filter(email=email).first()
+
+    if user:
+        add_user_to_accepted_teams(user)
+
+
 @receiver(user_signed_up)
-def add_user_to_team(request, user, **kwargs):
-    """
-    Adds the user to the team if there is invitation information in the URL.
-    """
-    invitation_id = get_invitation_id_from_request(request)
-    if invitation_id:
-        try:
-            invitation = Invite.objects.get(id=invitation_id)
-            process_invitation(invitation, user)
-        except Invite.DoesNotExist:
-            # for now just swallow missing invitation errors
-            # these should get picked up by the form validation
-            pass
+def add_user_to_team_after_signup(request, user, **kwargs):
+    add_user_to_accepted_teams(user)
