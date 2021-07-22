c04ef3d257e466c6708674cd3228abb9a132ef43
David Das Kell <davidambrosekell@gmail.com>
2021-07-22 20:05:08 +0100

    Add arity check using function inspection

--- lib/dag.py
+++ lib/dag.py
@@ -1,17 +1,34 @@
+import inspect
+
 from apps.nodes.models import Node
 from apps.nodes.nodes import NODE_FROM_CONFIG
+from ibis.expr.types import param
 
 
 def get_all_parents(node: Node):
+    # yield parents before child => topological order
     for parent in node.parents.all():
         yield from get_all_parents(parent)
     yield node
 
 
+def validate_arity(func, len_args):
+
+    _, *params = inspect.signature(func).parameters.values()
+
+    if len(params) == 0:
+        assert len_args == 0
+    # testing for "*args" in signature
+    elif any(param.kind == inspect.Parameter.VAR_POSITIONAL for param in params):
+        assert len_args >= len(params) - 1
+    else:
+        assert len_args == len(params)
+
+
 def get_query_from_node(node: Node):
 
     nodes = get_all_parents(node)
-    # python dicts are insertion ordered and unique
+    # remove duplicates (python dicts are insertion ordered)
     nodes = list(dict.fromkeys(nodes))
 
     results = {}
@@ -19,6 +36,9 @@ def get_query_from_node(node: Node):
     for node in nodes:
         func = NODE_FROM_CONFIG[node.kind]
         args = [results[parent] for parent in node.parents.all()]
-        results[node] = func(*args)
+
+        validate_arity(func, len(args))
+
+        results[node] = func(node, *args)
 
     return results[node]
