c5b26d8e6a75e0301927da5e62cd96f655608251
David Das Kell <davidambrosekell@gmail.com>
2021-07-22 23:53:15 +0100

    Sascha fix

--- lib/dag.py
+++ lib/dag.py
@@ -2,7 +2,6 @@ import inspect
 
 from apps.nodes.models import Node
 from apps.nodes.nodes import NODE_FROM_CONFIG
-from ibis.expr.types import param
 
 
 def get_all_parents(node: Node):
@@ -14,12 +13,10 @@ def get_all_parents(node: Node):
 
 def validate_arity(func, len_args):
 
-    _, *params = inspect.signature(func).parameters.values()
+    params = inspect.signature(func).parameters.values()
 
-    if len(params) == 0:
-        assert len_args == 0
     # testing for "*args" in signature
-    elif any(param.kind == inspect.Parameter.VAR_POSITIONAL for param in params):
+    if any(param.kind == inspect.Parameter.VAR_POSITIONAL for param in params):
         assert len_args >= len(params) - 1
     else:
         assert len_args == len(params)
