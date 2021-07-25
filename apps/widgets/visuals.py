92747111ad2ad45765b2981b108bc7eda421a721
David Das Kell <davidambrosekell@gmail.com>
2021-07-25 16:29:54 +0100

    charts in widgets

--- apps/widgets/visuals.py
+++ apps/widgets/visuals.py
@@ -3,11 +3,11 @@ from typing import Any, Dict
 
 from apps.filters.bigquery import create_filter_query
 from apps.integrations.bigquery import DEFAULT_LIMIT
-from lib.chart import to_chart
-from lib.clients import get_dataframe
 from apps.tables.bigquery import get_query_from_table
+from lib.clients import get_dataframe
 
 from .bigquery import query_widget
+from .chart import to_chart
 from .models import Widget
 
 
