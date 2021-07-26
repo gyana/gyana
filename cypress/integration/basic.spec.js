9dc31484d6f6edf14c55c72416eb718621e255d6
David Das Kell <davidambrosekell@gmail.com>
2021-07-26 16:46:05 +0100

    Fix test again

--- cypress/integration/basic.spec.js
+++ cypress/integration/basic.spec.js
@@ -19,15 +19,15 @@ describe('sign up', () => {
   it('signs up to app', () => {
     cy.visit('/accounts/signup')
 
-    cy.get('input[type=email]').type('test@gyana.com')
+    cy.get('input[type=email]').type('new@gyana.com')
     cy.get('input[type=password]').type('seewhatmatters')
 
     cy.contains('Create Account').click()
     cy.url().should('contain', '/teams/new')
 
-    cy.get('input[type=text]').type('Gyana')
+    cy.get('input[type=text]').type('New')
     cy.get('button[type=submit]').click()
 
-    cy.url().should('contain', '/teams/1')
+    cy.url().should('contain', '/teams/2')
   })
 })
