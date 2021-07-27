6409e6c199910cd99f56ee0c3a701853fe8471b1
David Das Kell <davidambrosekell@gmail.com>
2021-07-27 10:49:14 +0100

    Add a note on why we need endpoints

--- gyana/settings/cypress.py
+++ gyana/settings/cypress.py
@@ -13,4 +13,9 @@ DATABASES = {
     }
 }
 
+
+# URLs to reset and seed the database for testing. Although Cypress supports
+# running CLI commands, the overhead of starting up the python interpreter for
+# each ./manage.py command is 2-3s, whereas the actual reset/seed is 350ms.
+
 CYPRESS_URLS = True
