43c8ab9e5520e001fa9da27fe957076382209e0b
David Das Kell <davidambrosekell@gmail.com>
2021-07-27 15:35:02 +0100

    crud teams tests

--- cypress/integration/teams.spec.js
+++ cypress/integration/teams.spec.js
@@ -7,15 +7,40 @@ describe('teams', () => {
   it('create, read, update and delete', () => {
     cy.visit('/')
 
-    cy.contains('Vayu')
+    cy.get('#heading').within(() => cy.contains('Vayu'))
 
+    // create
     cy.get('#sidebar-newteam').click()
     cy.url().should('contain', '/teams/new')
 
     cy.get('input[type=text]').type('Neera')
     cy.get('button[type=submit]').click()
 
+    // view
     cy.url().should('contain', '/teams/2')
-    cy.contains('Neera')
+    cy.get('#heading').within(() => cy.contains('Neera'))
+
+    // switch
+    cy.get('#sidebar').within(() => {
+      cy.contains('Vayu').click()
+    })
+    cy.get('#heading').within(() => cy.contains('Vayu'))
+    cy.get('#sidebar').within(() => {
+      cy.contains('Neera').click()
+    })
+    cy.get('#heading').within(() => cy.contains('Neera'))
+
+    // update
+    cy.contains('Settings').click()
+    cy.url().should('contain', '/teams/2/update')
+    cy.get('input[type=text]').clear().type('Agni')
+    cy.get('button[type=submit]').click()
+    cy.get('#heading').within(() => cy.contains('Agni'))
+
+    // delete
+    cy.contains('Delete').click()
+    cy.url().should('contain', '/teams/2/delete')
+    cy.get('button[type=submit]').click()
+    cy.get('#sidebar').contains('Agni').should('not.exist')
   })
 })
