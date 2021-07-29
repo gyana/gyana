600c055a91db979c7daa195fb5214cb5d7566423
David Das Kell <davidambrosekell@gmail.com>
2021-07-29 13:28:00 +0100

    more tests

--- cypress/integration/integrations.spec.js
+++ cypress/integration/integrations.spec.js
@@ -6,6 +6,46 @@ describe('integrations', () => {
 
     cy.visit('/projects/1/integrations')
   })
+  it('view structure and data', () => {
+    cy.contains('store_info').click()
+
+    cy.url().should('contain', '/projects/1/integrations/1')
+
+    cy.contains('Structure').click()
+    cy.url().should('contain', '/projects/1/integrations/1/structure')
+    cy.get('table tbody tr').should('have.length', 4)
+    cy.get('table tbody :first-child').should('contain', 'store_id').and('contain', 'int64')
+
+    cy.contains('Data').click()
+    cy.url().should('contain', '/projects/1/integrations/1/data')
+    cy.get('table tbody tr').should('have.length', 15)
+    cy.get('table thead th').should('have.length', 4)
+  })
+  it('update and delete', () => {
+    cy.contains('store_info').click()
+
+    // update
+
+    cy.get('#tabbar').within(() => cy.contains('Settings').click())
+
+    cy.url().should('contain', '/projects/1/integrations/1/settings')
+    cy.get('#main').within(() => cy.get('input[name=name]').clear().type('Store Info'))
+    cy.get('button[type=submit]').click()
+
+    cy.url().should('contain', '/projects/1/integrations/1')
+    // the integration title is an editable input
+    cy.get('input[value="Store Info"]')
+
+    // delete
+
+    cy.get('#tabbar').within(() => cy.contains('Settings').click())
+
+    cy.contains('Delete').click()
+    cy.contains('Yes').click()
+
+    cy.url().should('contain', '/projects/1/integrations')
+    cy.get('table tbody tr').should('have.length', 0)
+  })
   it('upload valid CSV', () => {
     cy.contains('Upload CSV').click()
 
@@ -20,7 +60,7 @@ describe('integrations', () => {
 
     cy.url().should('contain', '/projects/1/integrations/2')
   })
-  it.only('connect to Google Sheet', () => {
+  it('connect to Google Sheet', () => {
     cy.contains('New Integration').click()
 
     cy.url().should('contain', '/projects/1/integrations/new')
@@ -30,7 +70,7 @@ describe('integrations', () => {
       'https://docs.google.com/spreadsheets/d/1mfauospJlft0B304j7em1vcyE1QKKVMhZjyLfIAnvmU/edit'
     )
     cy.get('input[name=name]').type('Stores info')
-    cy.get('input[name=cell_range]').type('A1:D11')
+    cy.get('input[name=cell_range]').type('store_info!A1:D11')
     cy.get('button[type=submit]').click()
 
     cy.url().should('contain', '/projects/1/integrations/2')
