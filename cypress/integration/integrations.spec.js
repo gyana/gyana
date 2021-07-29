c8557efa413d93466d74f19ba41b70b3a0d644d4
David Das Kell <davidambrosekell@gmail.com>
2021-07-29 14:00:02 +0100

    WIP fivetran test

--- cypress/integration/integrations.spec.js
+++ cypress/integration/integrations.spec.js
@@ -82,4 +82,21 @@ describe('integrations', () => {
     cy.contains('Data')
     cy.contains('10')
   })
+  it('connect to Fivetran', () => {
+    cy.contains('New Integration').click()
+
+    cy.url().should('contain', '/projects/1/integrations/new')
+    // all Fivetran connectors are mocked via MockFivetranClient
+    cy.contains('Google Analytics').click()
+
+    cy.url().should('contain', '/projects/1/integrations/create')
+    cy.get('input[name=name]').type('Google Analytics')
+    cy.get('button[type=submit]').click()
+
+    // the uuid is non-deterministic
+    cy.url().should('contain', '/projects/1/integrations/').and('contain', '/setup')
+    cy.get('button[type=submit]').click()
+
+    // TODO: test data is available
+  })
 })
