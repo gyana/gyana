a8a2200f2a4750234c2885ad93a9dfd28f8543ef
David Das Kell <davidambrosekell@gmail.com>
2021-07-30 16:12:56 +0100

    Finish off

--- apps/utils/javascript/stimulus.ts
+++ apps/utils/javascript/stimulus.ts
@@ -2,5 +2,19 @@ import { Application } from 'stimulus'
 import { definitionsFromContext } from 'stimulus/webpack-helpers'
 
 const application = Application.start()
-const context = require.context('./controllers', true, /\.js$/)
-application.load(definitionsFromContext(context))
+
+const APPS = [
+  'columns',
+  'dashboards',
+  'integrations',
+  'nodes',
+  'utils',
+  'web',
+  'widgets',
+  'workflows',
+]
+
+for (const app of APPS) {
+  const context = require.context(`../../${app}/javascript/controllers`, true, /\.js$/)
+  application.load(definitionsFromContext(context))
+}
