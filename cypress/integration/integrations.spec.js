b7e0853abe053232c8552d657d3816aad6adf94f
David Das Kell <davidambrosekell@gmail.com>
2021-08-06 15:06:49 +0100

    Fix integrations.spec.js mistake

--- cypress/integration/integrations.spec.js
+++ cypress/integration/integrations.spec.js
@@ -6,21 +6,44 @@ describe('integrations', () => {
 
     cy.visit('/projects/1/integrations')
   })
-  it('connect to Fivetran', () => {
-    cy.contains('New Integration').click()
+  it('view structure and data', () => {
+    cy.contains('store_info').click()
 
-    cy.url().should('contain', '/projects/1/integrations/new')
-    // all Fivetran connectors are mocked via MockFivetranClient
-    cy.contains('Google Analytics').click()
+    cy.url().should('contain', '/projects/1/integrations/1')
 
-    cy.url().should('contain', '/projects/1/integrations/create')
-    cy.get('input[name=name]').type('Google Analytics')
+    cy.contains('Structure').click()
+    cy.url().should('contain', '/projects/1/integrations/1/structure')
+    cy.get('table tbody tr').should('have.length', 4)
+    cy.get('table tbody :first-child').should('contain', 'store_id').and('contain', 'int64')
+
+    cy.contains('Data').click()
+    cy.url().should('contain', '/projects/1/integrations/1/data')
+    cy.get('table tbody tr').should('have.length', 15)
+    cy.get('table thead th').should('have.length', 4)
+  })
+  it('update and delete', () => {
+    cy.contains('store_info').click()
+
+    // update
+
+    cy.get('#tabbar').within(() => cy.contains('Settings').click())
+
+    cy.url().should('contain', '/projects/1/integrations/1/settings')
+    cy.get('#main').within(() => cy.get('input[name=name]').clear().type('Store Info'))
     cy.get('button[type=submit]').click()
 
-    // the uuid is non-deterministic
-    cy.url().should('contain', '/projects/1/integrations/').and('contain', '/setup')
-    cy.get('button[type=submit]').click()
+    cy.url().should('contain', '/projects/1/integrations/1')
+    // the integration title is an editable input
+    cy.get('input[value="Store Info"]')
 
-    // TODO: test data is available
+    // delete
+
+    cy.get('#tabbar').within(() => cy.contains('Settings').click())
+
+    cy.contains('Delete').click()
+    cy.contains('Yes').click()
+
+    cy.url().should('contain', '/projects/1/integrations')
+    cy.get('table tbody tr').should('have.length', 0)
   })
 })
