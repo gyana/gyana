495559ff844527fe21444e3c5294523db47ba684
David Das Kell <davidambrosekell@gmail.com>
2021-08-08 11:39:23 +0100

    Add breaking test

--- cypress/integration/sheets.spec.js
+++ cypress/integration/sheets.spec.js
@@ -17,11 +17,12 @@ describe('integrations', () => {
     cy.get('input[name=url]').type(
       'https://docs.google.com/spreadsheets/d/1mfauospJlft0B304j7em1vcyE1QKKVMhZjyLfIAnvmU/edit'
     )
-    cy.get('input[name=name]').type('Stores info')
     cy.get('input[name=cell_range]').type('store_info!A1:D11')
     cy.get('button[type=submit]').click()
 
     cy.url().should('contain', '/projects/1/integrations/2')
+    // Google Sheet name inferred
+    cy.contains('Store info sheet')
     cy.contains("Syncing, you'll get an email when it is ready")
     cy.contains('tasks processed')
     cy.contains('Reload to see results').click()
