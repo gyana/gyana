102ae5c5a5b4f807813ed3c932cb77433e763ab9
David Das Kell <davidambrosekell@gmail.com>
2021-08-09 15:54:37 +0100

    import table from upload

--- apps/uploads/bigquery.py
+++ apps/uploads/bigquery.py
@@ -1,5 +1,10 @@
+from apps.base.clients import bigquery_client
+from apps.tables.models import Table
 from django.conf import settings
 from google.cloud import bigquery
+from google.cloud.bigquery.job.load import LoadJob
+
+from .models import Upload
 
 
 def create_external_upload_config(file: str) -> bigquery.ExternalConfig:
@@ -19,3 +24,21 @@ def create_external_upload_config(file: str) -> bigquery.ExternalConfig:
     external_config.autodetect = True
 
     return external_config
+
+
+def import_table_from_upload(table: Table, upload: Upload) -> LoadJob:
+
+    client = bigquery_client()
+
+    job_config = bigquery.LoadJobConfig(
+        source_format=bigquery.SourceFormat.CSV,
+        autodetect=True,
+        skip_leading_rows=1,
+        # allow_quoted_newlines = True,
+        # external_config.options.allow_jagged_rows = True
+    )
+    uri = f"gs://{settings.GS_BUCKET_NAME}/{upload.file}"
+
+    load_job = client.load_table_from_uri(uri, table.bq_table, job_config=job_config)
+
+    return load_job
