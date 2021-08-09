3b6076be4ec5b92fa45a45c582de5fed6f157dcc
David Das Kell <davidambrosekell@gmail.com>
2021-08-09 21:11:54 +0100

    Fix basic upload test

--- cypress/integration/uploads.spec.js
+++ cypress/integration/uploads.spec.js
@@ -7,13 +7,19 @@ describe('uploads', () => {
     cy.visit('/projects/1/integrations')
   })
   it('upload valid CSV', () => {
+    cy.contains('New Integration').click()
     cy.contains('Upload CSV').click()
 
-    cy.url().should('contain', '/projects/1/integrations/uploads')
+    cy.url().should('contain', '/projects/1/integrations/uploads/new')
     cy.get('input[type=file]').attachFile('store_info.csv')
+    cy.contains('File uploaded')
     cy.get('button[type=submit]').click()
 
-    // bigquery file upload needs longer wait
+    cy.url().should('contain', '/projects/1/integrations/uploads/2')
+    cy.contains('Validating and importing your file...')
+    cy.contains('File successfully validated and imported.')
+
+    // // bigquery file upload needs longer wait
     cy.contains('Structure', { timeout: 10000 })
     cy.contains('Data')
     cy.contains('15')
