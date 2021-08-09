20dc2c8298effa679e25ff6bd72bc510a4dae521
David Das Kell <davidambrosekell@gmail.com>
2021-08-09 23:16:53 +0100

    working stream test

--- cypress/integration/uploads.spec.js
+++ cypress/integration/uploads.spec.js
@@ -30,25 +30,29 @@ describe('uploads', () => {
 
     // check email sent
     cy.outbox()
+
       .then((outbox) => outbox.count)
       .should('eq', 1)
   })
   it('streamed uploads with chunks', () => {
     cy.visit('/projects/1/integrations/uploads/new', {
       onBeforeLoad(window) {
-        // the total file size is 345 bytes
-        // requires 3 chunks to upload
-        window.__cypressChunkSize__ = 128
+        // minimum chunk size allowed "a multiple of 256 KiB (256 x 1024 bytes)"
+        // https://cloud.google.com/storage/docs/performing-resumable-uploads#chunked-upload
+        // by construction file is 1.1 MB = 2 chunks
+        window.__cypressChunkSize__ = 512 * 1024
       },
     })
 
-    cy.get('input[type=file]').attachFile('store_info.csv')
+    cy.get('input[type=file]').attachFile('fifa.csv')
     cy.contains('File uploaded')
     cy.get('button[type=submit]').click()
 
-    cy.contains('Structure', { timeout: 10000 })
+    cy.contains('Structure', { timeout: 20000 })
+    // 2250 lines of CSV including header
+    cy.contains(2249)
   })
-  it('validation failures', () => {
+  it.only('validation failures', () => {
     cy.contains('New Integration').click()
     cy.contains('Upload CSV').click()
 
