af779f1f6837ed936e61864374292a038182f70a
David Das Kell <davidambrosekell@gmail.com>
2021-08-18 12:44:35 +0100

    connectors and dashboards

--- cypress/support/utils.js
+++ cypress/support/utils.js
@@ -4,3 +4,6 @@ export const getModelStartId = (modelname) =>
   Math.max(
     ...fixtures.filter((fixture) => fixture.model == modelname).map((fixture) => fixture.pk)
   ) + 1
+
+export const getPendingIntegrations = () =>
+  fixtures.filter((fixture) => fixture.model == 'integrations.integration' && !fixture.fields.ready)
