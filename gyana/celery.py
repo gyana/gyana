d36e856a694e14986305fb79b3ee55403ea3d296
David Das Kell <davidambrosekell@gmail.com>
2021-08-20 12:36:55 +0100

    setup periodic task to delete pending integrations

--- gyana/celery.py
+++ gyana/celery.py
@@ -1,6 +1,7 @@
 import os
 
 from celery import Celery
+from celery.schedules import crontab
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gyana.settings.development")
@@ -15,3 +16,14 @@ app.config_from_object("django.conf:settings", namespace="CELERY")
 
 # Load task modules from all registered Django app configs.
 app.autodiscover_tasks()
+
+
+@app.on_after_configure.connect
+def setup_periodic_tasks(sender, **kwargs):
+    from apps.integrations.tasks import delete_outdated_pending_integrations
+
+    sender.add_periodic_task(
+        # run at midnight UTC every day
+        crontab(minute=0, hour=0),
+        delete_outdated_pending_integrations.s(),
+    )
