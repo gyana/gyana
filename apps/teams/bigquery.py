f2fc0627325bf2a92a73fdfc4700c8b50fc03365
David Das Kell <davidambrosekell@gmail.com>
2021-08-22 10:53:07 +0100

    fix teams and invites tests

--- apps/teams/bigquery.py
+++ apps/teams/bigquery.py
@@ -5,4 +5,5 @@ from .models import Team
 
 def create_team_dataset(team: Team):
     client = bigquery_client()
-    client.create_dataset(team.tables_dataset_id)
+    # exists ok is for testing
+    client.create_dataset(team.tables_dataset_id, exists_ok=True)
