c43b100af5b9ff8c44e646f3a6291b43d77cc2fc
David Das Kell <davidambrosekell@gmail.com>
2021-08-22 20:30:43 +0100

    inline table

--- apps/integrations/admin.py
+++ apps/integrations/admin.py
@@ -1,10 +1,31 @@
 from apps.connectors.models import Connector
 from apps.sheets.models import Sheet
+from apps.tables.models import Table
 from apps.uploads.models import Upload
 from django.contrib import admin
 from django.utils.html import format_html
 
 from .models import Integration
+
+
+class TableInline(admin.TabularInline):
+    model = Table
+    fields = ["bq_table", "bq_dataset", "bq_dashboard_url"]
+    readonly_fields = ["bq_table", "bq_dataset", "bq_dashboard_url"]
+    extra = 0
+
+    def bq_dashboard_url(self, instance):
+        return format_html(
+            '<a href="{0}" target="_blank">{1}</a>',
+            instance.bq_dashboard_url,
+            instance.bq_dashboard_url,
+        )
+
+    def has_add_permission(self, request, obj):
+        return False
+
+    def has_delete_permission(self, request, obj):
+        return False
 
 
 class ConnectorInline(admin.StackedInline):
@@ -60,7 +81,7 @@ class IntegrationAdmin(admin.ModelAdmin):
     readonly_fields = ["project", "kind", "state"]
 
     def get_inlines(self, request, obj):
-        return [KIND_TO_INLINE[obj.kind]]
+        return [KIND_TO_INLINE[obj.kind], TableInline]
 
     # This will help you to disbale add functionality
     def has_add_permission(self, request):
