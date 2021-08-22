785b7a7bec91e38d492c7ae722c5d9b212a57e66
David Das Kell <davidambrosekell@gmail.com>
2021-08-22 23:53:38 +0100

    comment out dashboard stuff

--- apps/base/dashboard.py
+++ apps/base/dashboard.py
@@ -58,35 +58,35 @@ class CustomIndexDashboard(Dashboard):
         # append a recent actions module
         self.children.append(modules.RecentActions(_("Recent Actions"), 5))
 
-        # append a feed module
-        self.children.append(
-            modules.Feed(
-                _("Latest Django News"),
-                feed_url="http://www.djangoproject.com/rss/weblog/",
-                limit=5,
-            )
-        )
+        # # append a feed module
+        # self.children.append(
+        #     modules.Feed(
+        #         _("Latest Django News"),
+        #         feed_url="http://www.djangoproject.com/rss/weblog/",
+        #         limit=5,
+        #     )
+        # )
 
-        # append another link list module for "support".
-        self.children.append(
-            modules.LinkList(
-                _("Support"),
-                children=[
-                    {
-                        "title": _("Django documentation"),
-                        "url": "http://docs.djangoproject.com/",
-                        "external": True,
-                    },
-                    {
-                        "title": _('Django "django-users" mailing list'),
-                        "url": "http://groups.google.com/group/django-users",
-                        "external": True,
-                    },
-                    {
-                        "title": _("Django irc channel"),
-                        "url": "irc://irc.freenode.net/django",
-                        "external": True,
-                    },
-                ],
-            )
-        )
+        # # append another link list module for "support".
+        # self.children.append(
+        #     modules.LinkList(
+        #         _("Support"),
+        #         children=[
+        #             {
+        #                 "title": _("Django documentation"),
+        #                 "url": "http://docs.djangoproject.com/",
+        #                 "external": True,
+        #             },
+        #             {
+        #                 "title": _('Django "django-users" mailing list'),
+        #                 "url": "http://groups.google.com/group/django-users",
+        #                 "external": True,
+        #             },
+        #             {
+        #                 "title": _("Django irc channel"),
+        #                 "url": "irc://irc.freenode.net/django",
+        #                 "external": True,
+        #             },
+        #         ],
+        #     )
+        # )
