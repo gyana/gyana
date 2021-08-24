267c6451a0c6c3eeed70465095550ac5f086a4e9
David Das Kell <davidambrosekell@gmail.com>
2021-08-24 23:34:47 +0100

    cleanup admin.py

--- apps/appsumo/admin.py
+++ apps/appsumo/admin.py
@@ -3,38 +3,37 @@ from django.contrib import admin
 from .models import AppsumoCode, PurchasedCodes, RefundedCodes, UploadedCodes
 
 
-@admin.register(RefundedCodes)
-class AppsumoCodeAdmin(admin.ModelAdmin):
+class BaseFileCodesAdmin(admin.ModelAdmin):
     list_display = ["data", "downloaded", "success"]
     fields = ["data", "downloaded", "success"]
     readonly_fields = ["success"]
+
+
+@admin.register(RefundedCodes)
+class AppsumoCodeAdmin(BaseFileCodesAdmin):
+    pass
 
 
 @admin.register(PurchasedCodes)
-class AppsumoCodeAdmin(admin.ModelAdmin):
-    list_display = ["data", "downloaded", "success"]
-    fields = ["data", "downloaded", "success"]
-    readonly_fields = ["success"]
+class AppsumoCodeAdmin(BaseFileCodesAdmin):
+    pass
 
 
 @admin.register(UploadedCodes)
-class AppsumoCodeAdmin(admin.ModelAdmin):
-    list_display = ["data", "downloaded", "success"]
-    fields = ["data", "downloaded", "success"]
-    readonly_fields = ["success"]
+class AppsumoCodeAdmin(BaseFileCodesAdmin):
+    pass
 
 
 @admin.register(AppsumoCode)
 class AppsumoCodeAdmin(admin.ModelAdmin):
-    list_display = ("code", "team", "redeemed")
-    fields = ["code", "team", "redeemed", "redeemed_by"]
+    list_display = ["code", "team", "redeemed"]
     readonly_fields = ["code", "redeemed", "redeemed_by"]
+    fields = readonly_fields + ["team"]
 
 
 class AppsumoCodeInline(admin.TabularInline):
     model = AppsumoCode
-    fields = ["code", "redeemed", "redeemed_by"]
-    readonly_fields = ["code", "redeemed", "redeemed_by"]
+    readonly_fields = fields = ["code", "redeemed", "redeemed_by"]
 
     def has_add_permission(self, request, obj):
         return False
