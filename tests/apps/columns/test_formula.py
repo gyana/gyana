b641f46a667cf9ca3b20faa1386675605c073846
saschahofmann <sascha.hofmann@gyana.co.uk>
2021-08-24 13:47:10 +0200

    more tests

--- tests/apps/columns/test_formula.py
+++ tests/apps/columns/test_formula.py
@@ -34,6 +34,22 @@ def create_int_unary_param(func_name, sql_name=None):
     )
 
 
+def create_datetime_unary_param(func_name, sql_name=None):
+    return pytest.param(
+        f"{func_name}(updated)",
+        BASE_QUERY.format(f"{sql_name or func_name}(`updated`)"),
+        id=func_name,
+    )
+
+
+def create_extract_unary_param(func_name, sql_name=None):
+    return pytest.param(
+        f"{func_name}(updated)",
+        BASE_QUERY.format(f"EXTRACT({sql_name or func_name} from `updated`)"),
+        id=func_name,
+    )
+
+
 @pytest.mark.parametrize(
     "formula, expected_sql",
     [
@@ -54,15 +70,21 @@ def create_int_unary_param(func_name, sql_name=None):
         ),
         # Test string operations
         create_str_unary_param("lower"),
+        create_str_unary_param("hash"),
+        pytest.param(
+            'join(", ", athlete, "that genius")',
+            BASE_QUERY.format("ARRAY_TO_STRING([`athlete`, 'that genius'], ', ')"),
+            id="join",
+        ),
         create_str_unary_param("upper"),
         create_str_unary_param("length"),
         create_str_unary_param("reverse"),
-        create_str_unary_param("strip"),
-        create_str_unary_param("lstrip"),
-        create_str_unary_param("rstrip"),
+        create_str_unary_param("strip", "trim"),
+        create_str_unary_param("lstrip", "ltrim"),
+        create_str_unary_param("rstrip", "rtrim"),
         pytest.param(
             'like(athlete, "Tom Daley")',
-            BASE_QUERY.format("`athlete` LIKE Tom Daley"),
+            BASE_QUERY.format("`athlete` LIKE 'Tom Daley'"),
             id="like",
         ),
         pytest.param(
@@ -87,15 +109,73 @@ def create_int_unary_param(func_name, sql_name=None):
         ),
         # Test numeric operations
         create_int_unary_param("abs"),
+        pytest.param(
+            "add(medals, stars)",
+            BASE_QUERY.format("`medals` + `stars`"),
+            id="add integer and float columns",
+        ),
+        pytest.param(
+            "medals + 5",
+            BASE_QUERY.format("`medals` + 5"),
+            id="add integer scalar to integer column",
+        ),
+        create_int_unary_param("ceil"),
+        pytest.param(
+            "divide(medals, stars)",
+            BASE_QUERY.format("IEEE_DIVIDE(`medals`, `stars`)"),
+            id="div integer and float columns",
+        ),
+        pytest.param(
+            "medals / 42",
+            BASE_QUERY.format("IEEE_DIVIDE(`medals`, 42)"),
+            id="div integer column and integer scalar",
+        ),
+        create_int_unary_param("exp"),
+        pytest.param(
+            "floor(stars)",
+            BASE_QUERY.format("CAST(FLOOR(`stars`) AS INT64)"),
+            id="floor",
+        ),
         create_int_unary_param("sqrt"),
         create_int_unary_param("ln"),
         pytest.param(
-            "log(medal, 3)",
-            BASE_QUERY.format("log(`medal`, 3)"),
+            "log(medals, 3)",
+            BASE_QUERY.format("log(`medals`, 3)"),
             id="log",
         ),
-        create_int_unary_param("log2"),
+        pytest.param(
+            "log2(medals)",
+            BASE_QUERY.format("log(`medals`, 2)"),
+            id="log2",
+        ),
         create_int_unary_param("log10"),
+        pytest.param(
+            "mul(stars, medals)",
+            BASE_QUERY.format("`stars` * `medals`"),
+            id="multiply int and float column",
+        ),
+        pytest.param(
+            "stars * 42",
+            BASE_QUERY.format("`stars` * 42"),
+            id="multiply float column and int scalar",
+        ),
+        pytest.param(
+            "pow(stars, medals)",
+            BASE_QUERY.format("pow(`stars`, `medals`)"),
+            id="float column to the power of int column",
+        ),
+        # Test datetime operations
+        create_extract_unary_param("year"),
+        create_datetime_unary_param("time", "TIME"),
+        create_datetime_unary_param("date", "DATE"),
+        create_extract_unary_param("second"),
+        create_extract_unary_param("month"),
+        create_extract_unary_param("minute"),
+        create_extract_unary_param("millisecond"),
+        create_extract_unary_param("hour"),
+        create_datetime_unary_param("epoch_seconds", "UNIX_SECONDS"),
+        create_datetime_unary_param("day_of_week"),
+        create_extract_unary_param("day"),
     ],
 )
 def test_formula(formula, expected_sql):
