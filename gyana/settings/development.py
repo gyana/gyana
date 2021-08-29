8507650ea37adddd615b6121d312f699c6cc02f4
David Das Kell <davidambrosekell@gmail.com>
2021-08-29 14:10:49 +0100

    remove custom database

--- gyana/settings/development.py
+++ gyana/settings/development.py
@@ -11,18 +11,6 @@ AUTH_PASSWORD_VALIDATORS = []
 # Enable cache busting locally.
 STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "cypress_gyana",
-        "USER": "postgres",
-        "PASSWORD": "***",
-        "HOST": "localhost",
-        "PORT": "5432",
-    }
-}
-
-
 # URLs to reset and seed the database for testing. Although Cypress supports
 # running CLI commands, the overhead of starting up the python interpreter for
 # each ./manage.py command is 2-3s, whereas the actual reset/seed is 350ms.
