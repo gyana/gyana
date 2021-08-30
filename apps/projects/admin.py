105813ef5e0e2fd90627537abf853b2fa7a8d97f
David Das Kell <davidambrosekell@gmail.com>
2021-08-30 10:22:52 +0100

    promote to template via admin

--- apps/projects/admin.py
+++ apps/projects/admin.py
@@ -6,12 +6,17 @@ from .models import Project
 
 @admin.register(Project)
 class ProjectAdmin(admin.ModelAdmin):
-    list_display = ["id", "name", "description", "team"]
+    list_display = ["id", "name", "description", "team", "is_template"]
     fields = ["id", "name", "description", "team"]
     readonly_fields = ["id"]
+    actions = ["promote_to_template"]
 
     @admin.action(description="Promote to template")
-    def make_published(self, request, queryset):
+    def promote_to_template(self, request, queryset):
         for project in queryset:
             template = Template(project=project)
             template.save()
+
+    @admin.display(boolean=True)
+    def is_template(self, object):
+        return hasattr(object, "template")
