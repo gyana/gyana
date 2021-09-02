ba9521f987d041e0dde1d728f6ba1fd4847a338a
David Das Kell <davidambrosekell@gmail.com>
2021-09-02 22:50:43 +0100

    computed derived data

--- apps/appsumo/tests/test_account.py
+++ apps/appsumo/tests/test_account.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 import pandas as pd
 import pytest
-from apps.appsumo.account import get_row_count
+from apps.appsumo.account import get_deal
 from apps.appsumo.models import AppsumoCode, PurchasedCodes, UploadedCodes
 from django.core.files.uploadedfile import SimpleUploadedFile
 from django.utils import timezone
@@ -16,6 +16,19 @@ PURCHASED_179 = "purchased-deal-179-usd.csv"
 PURCHASED_59 = "purchased-deal-59-usd.csv"
 
 M = 1_000_000
+
+# helper utility to generate from AppSumo point-in-time snapshots
+# !cp redeemed-snapshot-2021-06-25.csv redeemed-derived-49-usd.csv
+# generate_diff_csv("redeemed-snapshot-2021-06-25.csv", "redeemed-snapshot-2021-07-01.csv", "redeemed-derived-179-usd.csv")
+# generate_diff_csv("redeemed-snapshot-2021-07-01.csv", "redeemed-snapshot-2021-08-27.csv", "redeemed-derived-59-usd.csv")
+
+
+def generate_diff_csv(start, end, diff):
+    df_1 = pd.read_csv(APPSUMO_DATA_DIR / start, names=["code"]).code.tolist()
+    df_2 = pd.read_csv(APPSUMO_DATA_DIR / end, names=["code"]).code.tolist()
+    pd.DataFrame({"codes": list(set(df_2) - set(df_1))}).to_csv(
+        APPSUMO_DATA_DIR / diff, header=False, index=False
+    )
 
 
 @pytest.fixture
@@ -51,17 +64,17 @@ def test_deal_49_usd(setup_purchased_codes):
     # a user has a single code
     codes = AppsumoCode.objects.filter(code=purchased_49[0])
 
-    assert get_row_count(codes) == M
+    assert get_deal(codes) == M
 
     # a user stacks four codes
     codes = AppsumoCode.objects.filter(code__in=purchased_49[:4])
 
-    assert get_row_count(codes) == 10 * M
+    assert get_deal(codes) == 10 * M
 
     # a user stacks more than four codes
     codes = AppsumoCode.objects.filter(code__in=purchased_49[:10])
 
-    assert get_row_count(codes) == 10 * M
+    assert get_deal(codes) == 10 * M
 
 
 @pytest.mark.django_db
@@ -78,4 +91,4 @@ def test_deal_179_usd(setup_purchased_codes):
     # a user has a single code
     codes = AppsumoCode.objects.filter(code=purchased_179[0])
 
-    assert get_row_count(codes) == 5 * M
+    assert get_deal(codes) == 5 * M
