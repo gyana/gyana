db981e40f30a69cf6fbbd692f7a9723a1a348a08
David Das Kell <davidambrosekell@gmail.com>
2021-09-07 13:28:45 +0100

    etags for integration tables

--- apps/widgets/cache.py
+++ apps/widgets/cache.py
@@ -21,8 +21,8 @@ def etag_widget_output(request, project_id, dashboard_id, pk):
 # No cache header tells browser to always re-validate the resource
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/Caching#controlling_caching
 # https://web.dev/http-cache/#flowchart
-widget_output = cache_control(no_cache=True)(
+widget_output = lambda view: cache_control(no_cache=True)(
     condition(
         etag_func=etag_widget_output, last_modified_func=last_modified_widget_output
-    )
+    )(view)
 )
