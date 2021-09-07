49b17c3f157b945c90495c58437fd6e65935ae11
David Das Kell <davidambrosekell@gmail.com>
2021-09-07 21:46:42 +0100

    Temporarily set the crontab to run per minute, to validate heroku

--- apps/base/tasks.py
+++ apps/base/tasks.py
@@ -5,6 +5,5 @@ CHECK_IN_URL = "https://api.honeybadger.io/v1/check_in"
 
 
 def honeybadger_check_in(key):
-    print(settings.ENVIRONMENT)
-    # if settings.ENVIRONMENT == "local":
-    requests.get(f"{CHECK_IN_URL}/{key}")
+    if settings.ENVIRONMENT == "development":
+        requests.get(f"{CHECK_IN_URL}/{key}")
