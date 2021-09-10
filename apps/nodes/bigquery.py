fc5dbb89df618c04d6139eba96e2f5b0723bfb89
saschahofmann <sascha.hofmann@gyana.co.uk>
2021-09-10 13:49:51 +0200

    show error message for broken parents

--- apps/nodes/bigquery.py
+++ apps/nodes/bigquery.py
@@ -385,6 +385,14 @@ def get_query_from_node(node):
             logging.error(err, exc_info=err)
 
         # input node zero state
-        assert results[node] is not None
+        if results[node] is None:
+            raise NodeResultNone(node=node)
 
     return results[node]
+
+
+class NodeResultNone(Exception):
+    def __init__(self, node, *args: object) -> None:
+        super().__init__(*args)
+
+        self.node = node
