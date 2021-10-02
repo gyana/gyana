1a5869ac6c500957baaa0f7a388d11d7c4e76cb4
David Das Kell <davidambrosekell@gmail.com>
2021-10-02 13:23:20 +0100

    More tests

--- apps/teams/tests/test_integration.py
+++ apps/teams/tests/test_integration.py
@@ -1,4 +1,6 @@
 import pytest
+from apps.teams.models import Team
+from apps.users.models import CustomUser
 from pytest_django.asserts import assertRedirects
 
 pytestmark = pytest.mark.django_db
@@ -27,6 +29,8 @@ def test_team_crudl(client, logged_in_user):
     client.get(f"/teams/{team.id}")
     assertRedirects(client.get("/"), f"/teams/{team.id}")
 
+    # list -> NA
+
     # update
     assert client.get(f"/teams/{new_team.id}/update").status_code == 200
 
@@ -44,3 +48,64 @@ def test_team_crudl(client, logged_in_user):
     r.url == "/"
 
     assert logged_in_user.teams.count() == 1
+
+
+def test_member_crudl(client, logged_in_user):
+
+    team = logged_in_user.teams.first()
+    user = CustomUser.objects.create_user("member", onboarded=True)
+    team.members.add(user, through_defaults={"role": "admin"})
+    membership = user.membership_set.first()
+
+    # create -> invites
+    # read -> NA
+
+    # list
+    assert client.get(f"/teams/{team.id}/members/").status_code == 200
+
+    # update
+    assert (
+        client.get(f"/teams/{team.id}/members/{membership.id}/update").status_code
+        == 200
+    )
+
+    r = client.post(
+        f"/teams/{team.id}/members/{membership.id}/update", data={"role": "member"}
+    )
+    r.status_code == "301"
+    r.url == f"/teams/{team.id}/members/{membership.id}/update"
+    membership.refresh_from_db()
+    assert membership.role == "member"
+
+    # delete
+    client.get(f"/teams/{team.id}/members/{membership.id}/delete")
+
+    r = client.delete(f"/teams/{team.id}/members/{membership.id}/delete")
+    r.status_code == 301
+    r.url == f"/teams/{team.id}/members"
+
+    assert team.members.count() == 1
+
+
+def test_member_role_and_check_restricted_permissions(client, logged_in_user):
+
+    team = logged_in_user.teams.first()
+
+    assert client.get(f"/teams/{team.id}/members/").status_code == 200
+    assert client.get(f"/teams/{team.id}/invites/").status_code == 200
+    assert client.get(f"/teams/{team.id}/update").status_code == 200
+
+    # add a member
+    user = CustomUser.objects.create_user("member", onboarded=True)
+    team.members.add(user, through_defaults={"role": "member"})
+    client.force_login(user)
+
+    assert client.get(f"/teams/{team.id}/members/").status_code == 404
+    assert client.get(f"/teams/{team.id}/invites/").status_code == 404
+    assert client.get(f"/teams/{team.id}/update").status_code == 404
+
+def test_account_limit_warning(client, logged_in_user):
+    pass
+
+def test_account_limit_disabled(client, logged_in_user):
+    pass
