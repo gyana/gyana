e044f41d876116584dd1fb2253c02dac267f09c6
David Das Kell <davidambrosekell@gmail.com>
2021-10-03 12:26:11 +0100

    Integrations CRUDL

--- apps/integrations/tests/test_integration.py
+++ apps/integrations/tests/test_integration.py
@@ -1,17 +1,62 @@
 import pytest
-from apps.base.tests.asserts import assertFormRenders, assertLink, assertOK
-from apps.users.models import CustomUser
-from django.core import mail
-from pytest_django.asserts import assertContains, assertRedirects
+from apps.base.tests.asserts import (assertFormRenders, assertLink, assertOK,
+                                     assertSelectorLength)
+from apps.integrations.models import Integration
+from apps.projects.models import Project
+from pytest_django.asserts import assertRedirects
 
 pytestmark = pytest.mark.django_db
 
 
+def test_integration_crudl(client, logged_in_user):
+    team = logged_in_user.teams.first()
+    project = Project.objects.create(name="Project", team=team)
+    integration = Integration.objects.create(
+        project=project, kind=Integration.Kind.UPLOAD, name="store_info", ready=True
+    )
+    INTEGRATION_URL = f"/projects/{project.id}/integrations/{integration.id}"
+
+    # create -> special flow
+
+    # list
+    r = client.get(f"/projects/{project.id}")
+    assertOK(r)
+    assertLink(r, f"/projects/{project.id}/integrations/", "Integrations")
+
+    r = client.get(f"/projects/{project.id}/integrations/")
+    assertOK(r)
+    assertSelectorLength(r, "table tbody tr", 1)
+    assertLink(r, INTEGRATION_URL, "store_info")
+
+    # read
+    r = client.get(INTEGRATION_URL)
+    assertOK(r)
+    assertLink(r, f"{INTEGRATION_URL}/settings", "Settings")
+
+    # update
+    r = client.get(f"{INTEGRATION_URL}/settings")
+    assertOK(r)
+    assertFormRenders(r, ["name"])
+    assertLink(r, f"{INTEGRATION_URL}/delete", "Delete")
+
+    r = client.post(f"{INTEGRATION_URL}/settings", data={"name": "Store Info"})
+    assertRedirects(r, f"{INTEGRATION_URL}/settings", status_code=303)
+
+    integration.refresh_from_db()
+    assert integration.name == "Store Info"
+
+    # delete
+    r = client.get(f"{INTEGRATION_URL}/delete")
+    assertOK(r)
+    assertFormRenders(r)
+
+    r = client.delete(f"{INTEGRATION_URL}/delete")
+    assertRedirects(r, f"/projects/{project.id}/integrations/")
+
+    assert project.integration_set.count() == 0
+
+
 def test_structure_and_preview(client, logged_in_user):
-    pass
-
-
-def test_update_and_delete(client, logged_in_user):
     pass
 
 
