c8e75980c5cda20f85511046273e7239c44f1cbe
vallode <18506096+vallode@users.noreply.github.com>
2021-10-19 16:28:25 +0300

    Add fontawesome to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -37,6 +37,16 @@ module.exports = {
         test: /\.pcss$/i,
         use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'],
       },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?(#.*)?$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: '../fonts/',
+          },
+        },
+      },
     ],
   },
   plugins: [
