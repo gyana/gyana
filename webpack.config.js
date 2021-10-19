cdf8ef31c46250773ee60ead98f964b872685399
vallode <18506096+vallode@users.noreply.github.com>
2021-10-19 16:55:17 +0300

    Fix unintuative webpack paths

--- webpack.config.js
+++ webpack.config.js
@@ -43,7 +43,8 @@ module.exports = {
           loader: 'file-loader',
           options: {
             name: '[name].[ext]',
-            outputPath: '../fonts/',
+            outputPath: 'fonts/',
+            publicPath: '../fonts'
           },
         },
       },
