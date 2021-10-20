756e9b6e7675578e981fa2017eca62a3dfc563a2
David Das Kell <davidambrosekell@gmail.com>
2021-10-20 11:35:07 +0100

    table is added successfully

--- apps/connectors/tests/test_integration.py
+++ apps/connectors/tests/test_integration.py
@@ -1,10 +1,12 @@
-from unittest.mock import Mock
+from unittest.mock import MagicMock, Mock
 
 import pytest
 from apps.base.tests.asserts import assertFormRenders, assertLink, assertOK
 from apps.connectors.fivetran import FivetranSchema
 from apps.integrations.models import Integration
 from apps.projects.models import Project
+from google.cloud.bigquery.schema import SchemaField
+from google.cloud.bigquery.table import Table as BqTable
 from pytest_django.asserts import assertRedirects
 
 pytestmark = pytest.mark.django_db
@@ -15,7 +17,7 @@ def test_create(client, logged_in_user, bigquery_client, fivetran_client):
     team = logged_in_user.teams.first()
     project = Project.objects.create(name="Project", team=team)
 
-    # mock
+    # mock connector with a single table
     fivetran_client.create = lambda *_: {"fivetran_id": "fid", "schema": "sid"}
     fivetran_client.authorize = lambda c, r: f"http://fivetran.url?redirect_uri={r}"
     fivetran_client.has_completed_sync = lambda *_: True
@@ -32,6 +34,16 @@ def test_create(client, logged_in_user, bigquery_client, fivetran_client):
         tables={"table": table},
     )
     fivetran_client.get_schemas = lambda *_: [schema]
+    bigquery_client.list_tables = lambda *_: [
+        BqTable(
+            "project.dataset.table",
+            schema=[
+                SchemaField(column, type_)
+                for column, type_ in [("name", "STRING"), ("age", "INTEGER")]
+            ],
+        )
+    ]
+    bigquery_client.get_table().num_rows = 10
 
     # create a new connector, configure it and complete the sync
 
@@ -76,7 +88,7 @@ def test_create(client, logged_in_user, bigquery_client, fivetran_client):
     r = client.get(f"{INTEGRATION_URL}/configure")
     assertOK(r)
     # todo: fix this!
-    assertFormRenders(r, ["name", 'dataset_schema', 'dataset_tables'])
+    assertFormRenders(r, ["name", "dataset_schema", "dataset_tables"])
 
     # complete the sync
     # it will happen immediately as celery is run in eager mode
@@ -85,6 +97,8 @@ def test_create(client, logged_in_user, bigquery_client, fivetran_client):
 
     r = client.get(f"{INTEGRATION_URL}/load")
     assertRedirects(r, f"{INTEGRATION_URL}/done")
+
+    assert integration.table_set.count() == 1
 
     # todo: email
     # assert len(mail.outbox) == 1
