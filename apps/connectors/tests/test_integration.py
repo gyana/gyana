f9519c98d6102c526e4d9c5f8d1276ec3e11b88a
David Das Kell <davidambrosekell@gmail.com>
2021-10-20 15:41:20 +0100

    deleting tables

--- apps/connectors/tests/test_integration.py
+++ apps/connectors/tests/test_integration.py
@@ -9,6 +9,7 @@ from apps.connectors.fivetran import FivetranSchema
 from apps.connectors.models import Connector
 from apps.integrations.models import Integration
 from apps.projects.models import Project
+from apps.tables.models import Table
 from google.cloud.bigquery.schema import SchemaField
 from google.cloud.bigquery.table import Table as BqTable
 from pytest_django.asserts import assertRedirects
@@ -134,3 +135,75 @@ def test_status_on_pending_page(client, logged_in_user):
 
     integration.refresh_from_db()
     assert integration.state == Integration.State.DONE
+
+
+def test_update_tables_in_non_database(
+    client, logged_in_user, bigquery_client, fivetran_client
+):
+    team = logged_in_user.teams.first()
+    project = Project.objects.create(name="Project", team=team)
+    integration = Integration.objects.create(
+        project=project,
+        kind=Integration.Kind.CONNECTOR,
+        name="Google Analytics",
+        state=Integration.State.DONE,
+        ready=True,
+    )
+    connector = Connector.objects.create(
+        integration=integration,
+        service="google_analytics",
+        schema="schema",
+        fivetran_authorized=True,
+    )
+
+    table1 = {
+        "name_in_destination": "table1",
+        "enabled": True,
+        "enabled_patch_settings": {"allowed": True},
+    }
+    table2 = {
+        "name_in_destination": "table2",
+        "enabled": True,
+        "enabled_patch_settings": {"allowed": True},
+    }
+    schema = FivetranSchema(
+        key="schema",
+        name_in_destination="dataset",
+        enabled=True,
+        tables={"table1": table1, "table2": table2},
+    )
+    fivetran_client.get_schemas = lambda *_: [schema]
+
+    for table in [table1, table2]:
+        Table.objects.create(
+            project=project,
+            integration=integration,
+            source=Table.Source.INTEGRATION,
+            bq_table=table["name_in_destination"],
+            bq_dataset="dataset",
+        )
+
+    assert integration.table_set.count() == 2
+
+    # update the schema in fivetran
+    INTEGRATION_URL = f"/projects/{project.id}/integrations/{integration.id}"
+    r = client.get(f"{INTEGRATION_URL}/configure")
+    assertOK(r)
+    assertFormRenders(r, ["name", "dataset_tables", "dataset_schema"])
+
+    fivetran_client.get_schemas = lambda *_: [
+        FivetranSchema(
+            key="schema",
+            name_in_destination="dataset",
+            enabled=True,
+            tables={"table1": table1},
+        )
+    ]
+
+    r = client.post(f"{INTEGRATION_URL}/configure", data={"dataset_tables": ["table1"]})
+    assertRedirects(r, f"{INTEGRATION_URL}/load", target_status_code=302)
+    assert fivetran_client.update_schemas.call_count == 1
+    assert fivetran_client.update_schemas.call_args.args[1][0].tables[0].key == "table1"
+
+    # remove those tables
+    assert integration.table_set.count() == 1
