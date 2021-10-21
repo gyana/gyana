634a937430158711c052780175f2510fcccf976b
David Das Kell <davidambrosekell@gmail.com>
2021-10-21 17:30:28 +0100

    Refactor the fixtures

--- apps/base/tests/fixtures.py
+++ apps/base/tests/fixtures.py
@@ -8,10 +8,36 @@ from apps.users.models import CustomUser
 from waffle.templatetags import waffle_tags
 
 
+class BlankMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
+        return response
+
+
 @pytest.fixture(autouse=True)
-def patches(mocker):
+def patches(mocker, settings):
     mocker.patch("analytics.track")
     mocker.patch("apps.base.analytics.identify_user")
+
+    # enable beta
+    # https://waffle.readthedocs.io/en/v0.9/testing-waffles.html
+    mocker.patch.object(waffle, "flag_is_active", return_value=True)
+    mocker.patch.object(waffle_tags, "flag_is_active", return_value=True)
+
+    # run celery tasks within the same thread synchronously
+    settings.CELERY_TASK_ALWAYS_EAGER = True
+    settings.CELERY_TASK_EAGER_PROPAGATES = True
+
+    # all the clients are mocked
+    settings.MOCK_REMOTE_OBJECT_DELETION = False
+
+    # the test client does not have host header by default
+    mocker.patch("apps.cnames.middleware.HostMiddleware", BlankMiddleware)
+
+    yield
 
 
 @pytest.fixture(autouse=True)
@@ -44,6 +70,13 @@ def fivetran_client(*_, settings):
         yield client
 
 
+@pytest.fixture(autouse=True)
+def heroku_client(*_):
+    client = MagicMock()
+    with patch("apps.base.clients.heroku_client", return_value=client):
+        yield client
+
+
 @pytest.fixture
 def logged_in_user(client):
     team = Team.objects.create(name="team_team")
@@ -51,33 +84,3 @@ def logged_in_user(client):
     team.members.add(user, through_defaults={"role": "admin"})
     client.force_login(user)
     return user
-
-
-@pytest.fixture(autouse=True)
-def celery_eager(settings):
-    settings.CELERY_TASK_ALWAYS_EAGER = True
-    settings.CELERY_TASK_EAGER_PROPAGATES = True
-
-
-class BlankMiddleware:
-    def __init__(self, get_response):
-        self.get_response = get_response
-
-    def __call__(self, request):
-        response = self.get_response(request)
-        return response
-
-
-@pytest.fixture(autouse=True)
-def cname_middleware():
-    # the test client does not have host header by default
-    with patch("apps.cnames.middleware.HostMiddleware", BlankMiddleware):
-        yield
-
-
-@pytest.fixture(autouse=True)
-def enable_beta():
-    # https://waffle.readthedocs.io/en/v0.9/testing-waffles.html
-    with patch.object(waffle, "flag_is_active", return_value=True):
-        with patch.object(waffle_tags, "flag_is_active", return_value=True):
-            yield
