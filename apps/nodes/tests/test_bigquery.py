af935dfd116007626ededda8694fdf026140e238
saschahofmann <sascha.hofmann@gyana.co.uk>
2021-10-21 15:08:17 +0200

    more tests

--- apps/nodes/tests/test_bigquery.py
+++ apps/nodes/tests/test_bigquery.py
@@ -11,7 +11,8 @@ from apps.workflows.models import Workflow
 
 pytestmark = pytest.mark.django_db
 
-INPUT_QUERY = "SELECT *\nFROM `gyana-1511894275181.dataset.table`"
+TABLE_NAME = "gyana-1511894275181.dataset.table"
+INPUT_QUERY = f"SELECT *\nFROM `{TABLE_NAME}`"
 DEFAULT_X_Y = {"x": 0, "y": 0}
 
 
@@ -111,16 +112,128 @@ def test_aggregation_node(logged_in_user, bigquery_client):
     aggregation_node.parents.add(input_node)
 
     assert get_query_from_node(aggregation_node).compile() == INPUT_QUERY.replace(
-        "*", "count(*) as `count`"
+        "*", "count(*) AS `count`"
     )
 
     aggregation_node.aggregations.create(column="id", function="sum")
     assert get_query_from_node(aggregation_node).compile() == INPUT_QUERY.replace(
-        "*", "sum(`id`) as `id`"
+        "*", "sum(`id`) AS `id`"
     )
 
     aggregation_node.columns.create(column="birthday")
     assert (
         get_query_from_node(aggregation_node).compile()
-        == INPUT_QUERY.replace("*", "sum(`id`) as `id`") + "/nGROUP BY `birthday"
+        == INPUT_QUERY.replace("*", "`birthday`, sum(`id`) AS `id`") + "\nGROUP BY 1"
     )
+
+    aggregation_node.columns.create(column="athlete")
+    assert (
+        get_query_from_node(aggregation_node).compile()
+        == INPUT_QUERY.replace("*", "`birthday`, `athlete`, sum(`id`) AS `id`")
+        + "\nGROUP BY 1, 2"
+    )
+
+
+UNION_QUERY = (
+    f"SELECT `id`, `athlete`, `birthday`"
+    f"\nFROM (\n  SELECT *\n  FROM `{TABLE_NAME}`\n  UNION ALL"
+    f"\n  SELECT *\n  FROM `{TABLE_NAME}`\n) t0"
+)
+
+
+def test_union_node(logged_in_user, bigquery_client):
+    input_node, workflow = setup_input_node(logged_in_user, bigquery_client)
+    second_input_node = input_node.make_clone()
+
+    union_node = Node.objects.create(
+        kind=Node.Kind.UNION,
+        workflow=workflow,
+        **DEFAULT_X_Y,
+    )
+    union_node.parents.add(input_node, second_input_node)
+
+    assert get_query_from_node(union_node).compile() == UNION_QUERY
+
+    union_node.union_distinct = True
+    assert get_query_from_node(union_node).compile() == UNION_QUERY.replace(
+        "UNION ALL", "UNION DISTINCT"
+    )
+
+
+def test_except_node(logged_in_user, bigquery_client):
+    input_node, workflow = setup_input_node(logged_in_user, bigquery_client)
+    second_input_node = input_node.make_clone()
+
+    except_node = Node.objects.create(
+        kind=Node.Kind.EXCEPT,
+        workflow=workflow,
+        **DEFAULT_X_Y,
+    )
+    except_node.parents.add(input_node, second_input_node)
+
+    assert get_query_from_node(except_node).compile() == UNION_QUERY.replace(
+        "UNION ALL", "EXCEPT DISTINCT"
+    )
+
+
+def test_intersect_node(logged_in_user, bigquery_client):
+    input_node, workflow = setup_input_node(logged_in_user, bigquery_client)
+    second_input_node = input_node.make_clone()
+
+    intersect_node = Node.objects.create(
+        kind=Node.Kind.INTERSECT,
+        workflow=workflow,
+        **DEFAULT_X_Y,
+    )
+    intersect_node.parents.add(input_node, second_input_node)
+
+    assert get_query_from_node(intersect_node).compile() == UNION_QUERY.replace(
+        "UNION ALL", "INTERSECT DISTINCT"
+    )
+
+
+def test_sort_node(logged_in_user, bigquery_client):
+    input_node, workflow = setup_input_node(logged_in_user, bigquery_client)
+
+    sort_node = Node.objects.create(
+        kind=Node.Kind.SORT,
+        workflow=workflow,
+        **DEFAULT_X_Y,
+    )
+    sort_node.parents.add(input_node)
+
+    sort_node.sort_columns.create(column="id")
+    sort_query = f"{INPUT_QUERY}\nORDER BY `id`"
+    assert get_query_from_node(sort_node).compile() == sort_query
+
+    sort_node.sort_columns.create(column="birthday", ascending=False)
+    assert get_query_from_node(sort_node).compile() == sort_query + ", `birthday` DESC"
+
+
+def test_limit_node(logged_in_user, bigquery_client):
+    input_node, workflow = setup_input_node(logged_in_user, bigquery_client)
+
+    limit_node = Node.objects.create(
+        kind=Node.Kind.LIMIT,
+        workflow=workflow,
+        **DEFAULT_X_Y,
+    )
+    limit_node.parents.add(input_node)
+
+    limit_query = (
+        f"SELECT `id`, `athlete`, `birthday`"
+        f"\nFROM (\n  SELECT *\n  FROM `{TABLE_NAME}`"
+        f"\n  LIMIT 100\n) t0"
+    )
+    assert get_query_from_node(limit_node).compile() == limit_query
+
+    limit_node.limit_offset = 50
+    limit_node.limit_limit = 250
+
+    assert get_query_from_node(limit_node).compile() == limit_query.replace(
+        "LIMIT 100", "LIMIT 250 OFFSET 50"
+    )
+
+
+def test_filter_node(logged_in_user, bigquery_client):
+    assert False
