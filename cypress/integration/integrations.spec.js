1e23e39684fbe6634af5044400842e96fabdbaac
David Das Kell <davidambrosekell@gmail.com>
2021-10-21 21:31:01 +0100

    Cleanup integrations tests

--- cypress/integration/integrations.spec.js
+++ cypress/integration/integrations.spec.js
@@ -1,13 +1,18 @@
 /// <reference types="cypress" />
 
-import { readyIntegrations, pendingIntegrations, BIGQUERY_TIMEOUT } from '../support/utils'
+import { BIGQUERY_TIMEOUT } from '../support/utils'
 
 const SHARED_SHEET =
   'https://docs.google.com/spreadsheets/d/1mfauospJlft0B304j7em1vcyE1QKKVMhZjyLfIAnvmU/edit'
 
 describe('integrations', () => {
   it('create a sheet integration with retry', () => {
+    cy.login()
     cy.visit('/projects/1')
+
+    // todo: zero state
+    cy.contains('2 integrations').click()
+    cy.url().should('contain', '/projects/1/integrations')
 
     cy.contains('New Integration').click()
     cy.contains('Add Sheet').click()
@@ -21,20 +26,8 @@ describe('integrations', () => {
     cy.get('button[type=submit]').click()
     cy.contains('No columns found in the schema.', { timeout: BIGQUERY_TIMEOUT })
 
-    // check the pending page and navigate back
-    cy.visit('/projects/1/integrations')
-    cy.get('table tbody tr').should('have.length', readyIntegrations)
-
-    cy.contains(`Pending (${1 + pendingIntegrations})`).click()
-    cy.url().should('contain', '/projects/1/integrations/pending')
-    cy.get('table tbody tr').should('have.length', 1 + pendingIntegrations)
-    cy.contains('Store info sheet').click()
-
     // try to retry it
     cy.contains('Retry').click()
-
-    // it fails again
-    cy.contains('No columns found in the schema.', { timeout: BIGQUERY_TIMEOUT })
 
     // edit the configuration
     cy.get('#main').within(() => cy.contains('Configure').click())
@@ -42,12 +35,9 @@ describe('integrations', () => {
     cy.get('button[type=submit]').click()
 
     cy.contains('Confirm', { timeout: BIGQUERY_TIMEOUT }).click()
+    // only 10/15 rows imported
+    cy.contains('10 Rows')
 
-    // check the pending page again
-    cy.visit('/projects/1/integrations')
-    cy.get('table tbody tr').should('have.length', readyIntegrations + 1)
-
-    cy.contains('Pending').click()
-    cy.get('table tbody tr').should('have.length', pendingIntegrations)
+    // todo: next step in the flow
   })
 })
