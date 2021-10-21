20cc4de3ad171bbfc068da042fdff747050b8ff3
saschahofmann <sascha.hofmann@gyana.co.uk>
2021-10-21 14:11:51 +0200

    join and select and aggregation test

--- apps/nodes/tests/test_bigquery.py
+++ apps/nodes/tests/test_bigquery.py
@@ -1,8 +1,7 @@
-from unittest.mock import patch
-
 import pytest
 from apps.base.tests.mock_data import TABLE
 from apps.base.tests.mocks import mock_bq_client_with_schema
+from apps.columns.models import Column
 from apps.integrations.models import Integration
 from apps.nodes.bigquery import get_query_from_node
 from apps.nodes.models import Node
@@ -33,7 +32,7 @@ def setup_input_node(logged_in_user, bigquery_client):
 
     mock_bq_client_with_schema(
         bigquery_client,
-        [(column, type_.name) for column, type_ in TABLE.schema().items()],
+        [(column, type_.name) for column, type_ in TABLE.schema().items()][:3],
     )
     return (
         Node.objects.create(
@@ -58,3 +57,70 @@ def test_ouput_node(logged_in_user, bigquery_client):
     query = get_query_from_node(output_node)
 
     assert query.compile() == INPUT_QUERY
+
+
+def test_select_node(logged_in_user, bigquery_client):
+    input_node, workflow = setup_input_node(logged_in_user, bigquery_client)
+    select_node = Node.objects.create(
+        kind=Node.Kind.SELECT, workflow=workflow, **DEFAULT_X_Y
+    )
+    select_node.parents.add(input_node)
+    select_node.columns.add(
+        Column(column="athlete"), Column(column="birthday"), bulk=False
+    )
+
+    query = get_query_from_node(select_node)
+    assert query.compile() == INPUT_QUERY.replace("*", "`athlete`, `birthday`")
+
+    select_node.select_mode = "exclude"
+    query = get_query_from_node(select_node)
+    assert query.compile() == INPUT_QUERY.replace("*", "`id`")
+
+
+def test_join_node(logged_in_user, bigquery_client):
+    input_node, workflow = setup_input_node(logged_in_user, bigquery_client)
+    second_input_node = input_node.make_clone()
+    join_node = Node.objects.create(
+        kind=Node.Kind.JOIN,
+        workflow=workflow,
+        **DEFAULT_X_Y,
+        join_left="id",
+        join_right="id",
+    )
+    join_node.parents.add(input_node, second_input_node)
+
+    query = get_query_from_node(join_node)
+    # Mocking the table conditionally requires a little bit more work
+    # So we simply join the table with itself which leads to duplicate columns that
+    # Are aliased
+    join_query = "SELECT `id_left` AS `id`, `athlete_left`, `birthday_left`, `athlete_right`,\n       `birthday_right`\nFROM (\n  SELECT *\n  FROM (\n    SELECT `id` AS `id_left`, `athlete` AS `athlete_left`,\n           `birthday` AS `birthday_left`\n    FROM `gyana-1511894275181.dataset.table`\n  ) t1\n    INNER JOIN (\n      SELECT `id` AS `id_right`, `athlete` AS `athlete_right`,\n             `birthday` AS `birthday_right`\n      FROM `gyana-1511894275181.dataset.table`\n    ) t2\n      ON t1.`id_left` = t2.`id_right`\n) t0"
+    assert query.compile() == join_query
+
+    join_node.join_how = "outer"
+    query = get_query_from_node(join_node)
+    assert query.compile() == join_query.replace("INNER", "FULL OUTER")
+
+
+def test_aggregation_node(logged_in_user, bigquery_client):
+    input_node, workflow = setup_input_node(logged_in_user, bigquery_client)
+    aggregation_node = Node.objects.create(
+        kind=Node.Kind.AGGREGATION,
+        workflow=workflow,
+        **DEFAULT_X_Y,
+    )
+    aggregation_node.parents.add(input_node)
+
+    assert get_query_from_node(aggregation_node).compile() == INPUT_QUERY.replace(
+        "*", "count(*) as `count`"
+    )
+
+    aggregation_node.aggregations.create(column="id", function="sum")
+    assert get_query_from_node(aggregation_node).compile() == INPUT_QUERY.replace(
+        "*", "sum(`id`) as `id`"
+    )
+
+    aggregation_node.columns.create(column="birthday")
+    assert (
+        get_query_from_node(aggregation_node).compile()
+        == INPUT_QUERY.replace("*", "sum(`id`) as `id`") + "/nGROUP BY `birthday"
+    )
