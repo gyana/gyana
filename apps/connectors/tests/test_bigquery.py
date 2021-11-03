c1eaff382d72fd96677f102e7d59b7043baab048
David Das Kell <davidambrosekell@gmail.com>
2021-11-03 23:58:49 +0000

    finish test_bigquery

--- apps/connectors/tests/test_bigquery.py
+++ apps/connectors/tests/test_bigquery.py
@@ -10,7 +10,7 @@ from .mock import get_mock_list_tables, get_mock_schema
 pytestmark = pytest.mark.django_db
 
 
-def _get_bq_ids_from_bq_tables(bq_tables):
+def _get_bq_ids(bq_tables):
     return {f"{t.dataset_id}.{t.table_id}" for t in bq_tables}
 
 
@@ -28,7 +28,7 @@ def test_get_bq_tables_for_connector_event_tracking(
 
     bq_tables = get_bq_tables_for_connector(connector)
 
-    assert _get_bq_ids_from_bq_tables(bq_tables) == {
+    assert _get_bq_ids(bq_tables) == {
         "dataset.table_1",
         "dataset.table_2",
         "dataset.table_3",
@@ -54,50 +54,68 @@ def test_get_bq_tables_for_connector_webhooks_reports(
 
     bigquery.get_table = lambda x: raise_(NotFound("not found"))
     bq_tables = get_bq_tables_for_connector(connector)
-    assert _get_bq_ids_from_bq_tables(bq_tables) == set()
+    assert _get_bq_ids(bq_tables) == set()
 
     bigquery.get_table = Mock(return_value=BqTable("project.dataset.webhooks_table"))
     bq_tables = get_bq_tables_for_connector(connector)
-    assert _get_bq_ids_from_bq_tables(bq_tables) == {"dataset.webhooks_table"}
+    assert _get_bq_ids(bq_tables) == {"dataset.webhooks_table"}
 
     assert bigquery.get_table.call_count == 1
     assert bigquery.get_table.call_args.args == ("dataset.webhooks_table",)
 
 
-def test_get_bq_tables_for_connector_api_cloud(bigquery):
+def test_get_bq_tables_for_connector_api_cloud(
+    bigquery, connector_factory, logged_in_user
+):
+
+    schema_obj = get_mock_schema(3, "google_analytics", disabled=[2])
+    connector = connector_factory(
+        service="google_analytics",
+        schema_config=schema_obj.to_dict(),
+        integration__project__team=logged_in_user.teams.first(),
+    )
 
     # test: api_cloud maps the intersection of bigquery and enabled tables in fivetran
 
     # table_2 is disabled in fivetran and should be ignored
-    schema_obj = get_mock_schema(3, "google_analytics", disabled=[2])
     bigquery.list_tables.return_value = get_mock_list_tables(0)
-
-    assert schema_obj.get_bq_ids() == set()
+    bq_tables = get_bq_tables_for_connector(connector)
+    assert _get_bq_ids(bq_tables) == set()
 
     # table_4 in bigquery is not in fivetran schema and should be ignored
     bigquery.list_tables.return_value = get_mock_list_tables(4)
     bigquery.list_tables.reset_mock()
 
-    assert schema_obj.get_bq_ids() == {"dataset.table_1", "dataset.table_3"}
+    bq_tables = get_bq_tables_for_connector(connector)
+    assert _get_bq_ids(bq_tables) == {"dataset.table_1", "dataset.table_3"}
 
     assert bigquery.list_tables.call_count == 1
     assert bigquery.list_tables.call_args.args == ("dataset",)
 
 
-def test_get_bq_tables_for_connector_database(bigquery):
-
-    # test: api_cloud maps the intersection of bigquery and enabled tables in fivetran
+def test_get_bq_tables_for_connector_database(
+    bigquery, connector_factory, logged_in_user
+):
 
     # table_2 and schema_2 is disabled in fivetran and should be ignored
     schema_obj = get_mock_schema(
         3, "postgres", disabled=[2], num_schemas=3, schemas_disabled=[2]
     )
+    connector = connector_factory(
+        service="postgres",
+        schema_config=schema_obj.to_dict(),
+        integration__project__team=logged_in_user.teams.first(),
+    )
+
+    # test: api_cloud maps the intersection of bigquery and enabled tables in fivetran
+
     # table_4 in bigquery is not in fivetran schema and should be ignored
     bigquery.list_tables.side_effect = lambda dataset_id: get_mock_list_tables(
         4, dataset_id
     )
 
-    assert schema_obj.get_bq_ids() == {
+    bq_tables = get_bq_tables_for_connector(connector)
+    assert _get_bq_ids(bq_tables) == {
         "dataset_schema_1.table_1",
         "dataset_schema_1.table_3",
         "dataset_schema_3.table_1",
