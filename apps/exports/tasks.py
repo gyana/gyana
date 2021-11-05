694859d4b7cb6cfd45f8566c2f2844a1eebcb1af
vallode <18506096+vallode@users.noreply.github.com>
2021-11-05 13:22:57 +0200

    Add email templates to exports

--- apps/exports/tasks.py
+++ apps/exports/tasks.py
@@ -1,15 +1,18 @@
 from datetime import datetime as dt
 
+from celery.app import shared_task
+from django.conf import settings
+from django.core.mail.message import EmailMultiAlternatives
+from django.db import transaction
+from django.template import Context
+from django.template.loader import get_template
+from google.cloud import bigquery
+
 from apps.base import clients
 from apps.base.utils import short_hash
 from apps.nodes.bigquery import get_query_from_node
 from apps.tables.models import Table
 from apps.users.models import CustomUser
-from celery.app import shared_task
-from django.conf import settings
-from django.core.mail.message import EmailMessage
-from django.db import transaction
-from google.cloud import bigquery
 
 from .models import Export
 
@@ -44,15 +47,22 @@ def export_to_gcs(export_id, user_id):
     )
     extract_job = client.extract_table(table.bq_id, f"gs://{gcs_path}")
     extract_job.result()
+
     with transaction.atomic():
         export.gcs_path = gcs_path
         export.status = Export.Status.EXPORTED
         export.save()
 
-    message = EmailMessage(
-        subject="Your export is ready",
-        body=f"Click the link to download <a href='https://storage.cloud.google.com/{gcs_path}'>download</a>.",
-        from_email="Gyana Notifications <notifications@gyana.com>",
-        to=[user.email],
+    message_template_plain = get_template("exports/email/export_ready_message.txt")
+    message_template = get_template("exports/email/export_ready_message.html")
+
+    message = EmailMultiAlternatives(
+        "Your export is ready",
+        message_template_plain.render({"user": user, "gcs_path": gcs_path}),
+        "Gyana Notifications <notifications@gyana.com>",
+        [user.email],
+    )
+    message.attach_alternative(
+        message_template.render({"user": user, "gcs_path": gcs_path}), "text/html"
     )
     message.send()
