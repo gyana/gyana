ffc01328348b2a2d31a5bbd925d31c00fb3bdd42
David Das Kell <davidambrosekell@gmail.com>
2021-11-11 01:58:30 +0000

    Fix third party djpaddle migrations

--- gyana/settings/base.py
+++ gyana/settings/base.py
@@ -424,3 +424,6 @@ DJPADDLE_LINK_STALE_SUBSCRIPTIONS = False
 
 DJPADDLE_PRO_PLAN_ID = int(os.getenv("DJPADDLE_PRO_PLAN_ID", default="0"))
 DJPADDLE_BUSINESS_PLAN_ID = int(os.getenv("DJPADDLE_BUSINESS_PLAN_ID", default="0"))
+
+# https://stackoverflow.com/questions/47153776/how-to-store-third-party-apps-migrations-in-django
+MIGRATION_MODULES = {"djpaddle": "apps.teams.migrate.djpaddle"}
