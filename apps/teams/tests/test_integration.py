bc1f061b1175bc11a8e7c36347613517cc3862af
David Das Kell <davidambrosekell@gmail.com>
2021-11-14 02:59:40 +0000

    Fix tests

--- apps/teams/tests/test_integration.py
+++ apps/teams/tests/test_integration.py
@@ -47,13 +47,13 @@ def test_team_crudl(client, logged_in_user, bigquery, settings):
     r = client.post("/teams/new", data={"name": "Neera"})
     assert logged_in_user.teams.count() == 2
     new_team = logged_in_user.teams.first()
-    assertRedirects(r, f"/teams/{new_team.id}/plan", status_code=303)
+    assertRedirects(r, f"/teams/{new_team.id}/plans", status_code=303)
 
     assert bigquery.create_dataset.call_count == 1
     assert bigquery.create_dataset.call_args.args == (new_team.tables_dataset_id,)
 
     # choose plan
-    r = client.get(f"/teams/{new_team.id}/plan")
+    r = client.get(f"/teams/{new_team.id}/plans")
     assertOK(r)
     assertLink(r, f"/teams/{new_team.id}", "Continue")
 
