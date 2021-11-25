43faa95d2a014b59b1cdbefbfc50bcd8fedde9b6
David Das Kell <davidambrosekell@gmail.com>
2021-11-25 23:30:46 +0000

    Update workflow run button logic

--- apps/workflows/javascript/RunButton.tsx
+++ apps/workflows/javascript/RunButton.tsx
@@ -23,76 +23,78 @@ const RunButton: React.FC<{
   isOutOfDate,
   setIsOutOfDate,
 }) => {
-    const [loading, setLoading] = useState(false)
-    useEffect(() => {
-      const update = () => setIsOutOfDate(true)
+  const [loading, setLoading] = useState(false)
+  useEffect(() => {
+    const update = () => setIsOutOfDate(true)
 
-      window.addEventListener(GyanaEvents.UPDATE_WORKFLOW, update)
-      return () => window.removeEventListener(GyanaEvents.UPDATE_WORKFLOW, update)
-    })
+    window.addEventListener(GyanaEvents.UPDATE_WORKFLOW, update)
+    return () => window.removeEventListener(GyanaEvents.UPDATE_WORKFLOW, update)
+  })
 
-    const disabled = !hasOutput || loading || !isOutOfDate
-    return (
-      <div className='dndflow__run-button'>
-        <button
-          onClick={() => {
-            if (!disabled) {
-              setLoading(true)
+  const disabled = !hasOutput || loading || !isOutOfDate
+  return (
+    <div className='dndflow__run-button'>
+      <button
+        onClick={() => {
+          if (!disabled) {
+            setLoading(true)
 
-              client
-                .action(window.schema, ['workflows', 'run_workflow', 'create'], {
-                  id: workflowId,
-                })
-                .then((res) => {
-                  if (res) {
-                    setElements(
-                      elements.map((el) => {
-                        if (isNode(el)) {
-                          const error = res[parseInt(el.id)]
-                          // Add error to node if necessary
-                          if (error) {
-                            el.data['error'] = error
-                          }
-                          // Remove error if necessary
-                          else if (el.data.error) {
-                            delete el.data['error']
-                          }
+            client
+              .action(window.schema, ['workflows', 'run_workflow', 'create'], {
+                id: workflowId,
+              })
+              .then((res) => {
+                if (res) {
+                  setElements(
+                    elements.map((el) => {
+                      if (isNode(el)) {
+                        const error = res[parseInt(el.id)]
+                        // Add error to node if necessary
+                        if (error) {
+                          el.data['error'] = error
                         }
-                        return el
-                      })
-                    )
-                    if (Object.keys(res).length === 0) {
-                      setIsOutOfDate(false)
-                      setHasBeenRun(false)
-                      window.dispatchEvent(new Event(GyanaEvents.RUN_WORKFLOW))
-                    }
-                    setLoading(false)
+                        // Remove error if necessary
+                        else if (el.data.error) {
+                          delete el.data['error']
+                        }
+                      }
+                      return el
+                    })
+                  )
+                  if (Object.keys(res).length === 0) {
+                    setIsOutOfDate(false)
+                    setHasBeenRun(false)
+                    window.dispatchEvent(new Event(GyanaEvents.RUN_WORKFLOW))
                   }
-                  alert('Workflow finished running!')
-                })
-                .catch(() => {
                   setLoading(false)
-                  alert('Workflow failed running')
-                })
-            }
-          }}
-          className={`button button--sm button--success ${disabled && 'disabled'}`}
-          data-controller='tooltip'
-        >
-          {loading ? (
-            <i className='fad fa-fw fa-spinner-third fa-spin' />
-          ) : (<i className='fas fa-fw fa-play'></i>)}
-          Run
-          <div data-tooltip-target='body'>
-            {!hasOutput && hasBeenRun ? (
-              <span>Workflow needs output node to run</span>
-            ) : (
-              <span>Run a workflow to use results in dashboards and other workflows.</span>
-            )}
-          </div>
-        </button>
-      </div>
-    )
-  }
+                }
+                alert('Workflow finished running!')
+              })
+              .catch(() => {
+                setLoading(false)
+                alert('Workflow failed running')
+              })
+          }
+        }}
+        className={`button button--sm button--success ${disabled && 'disabled'}`}
+        data-controller='tooltip'
+      >
+        {loading ? (
+          <i className='fad fa-fw fa-spinner-third fa-spin' />
+        ) : (
+          <i className='fas fa-fw fa-play'></i>
+        )}
+        Run
+        <div data-tooltip-target='body'>
+          {!hasOutput ? (
+            <span>Workflow needs a Save Data node to run</span>
+          ) : (
+            <span>Run a workflow to use results in dashboards and other workflows.</span>
+          )}
+        </div>
+      </button>
+    </div>
+  )
+}
 
 export default RunButton
