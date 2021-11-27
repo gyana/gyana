ddad5e3a7e94d9e2df39509de4c20cbdd906966b
David Das Kell <davidambrosekell@gmail.com>
2021-11-27 14:21:40 +0000

    Remove serde.ts

--- apps/workflows/javascript/actions.ts
+++ apps/workflows/javascript/actions.ts
@@ -1,6 +1,39 @@
 import { getApiClient } from 'apps/base/javascript/api'
-import { Node, Edge, XYPosition, Connection } from 'react-flow-renderer'
-import { toEdge, toNode } from './serde'
+import { Node, Edge, XYPosition, Connection, ArrowHeadType } from 'react-flow-renderer'
+import { NODES } from './interfaces'
+
+// Utilities to convert from coreapi JSON response to react-flow-renderer
+
+export const toNode = (res, position: XYPosition): Node => ({
+  id: `${res.id}`,
+  type: ['input', 'output', 'text'].includes(res.kind) ? res.kind : 'default',
+  data: {
+    label: res.name || NODES[res.kind].displayName,
+    icon: NODES[res.kind].icon,
+    kind: res.kind,
+    error: res.error,
+    ...(res.kind === 'text' ? { text: res.text_text } : {}),
+    description: res.description,
+  },
+  position,
+})
+
+export const toEdge = (id: number, parent: number, child: number, position: number): Edge => {
+  return {
+    id: `reactflow__edge-${parent}${position}-${child}null`,
+    source: parent.toString(),
+    sourceHandle: null,
+    type: 'smoothstep',
+    targetHandle: parent.toString(),
+    arrowHeadType: ArrowHeadType.ArrowClosed,
+    target: child.toString(),
+    data: {
+      id,
+    },
+  }
+}
+
+// CRUDL REST API operations for nodes and edges
 
 const client = getApiClient()
 
