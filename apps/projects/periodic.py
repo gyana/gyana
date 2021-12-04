3a17d7345db9bdaab02f87cf15db4524597c81ee
David Das Kell <davidambrosekell@gmail.com>
2021-12-04 00:33:25 +0000

    add retries for connectors

--- apps/projects/periodic.py
+++ apps/projects/periodic.py
@@ -12,6 +12,11 @@ from apps.workflows.models import Workflow
 
 from .models import Project
 
+# Retry every 10 minutes for next 6 hours, this will continue to try until
+# the incremental connector resyncs are completed.
+RETRY_COUNTDOWN = 60 * 10
+MAX_RETRIES = 3600 / RETRY_COUNTDOWN * 24
+
 
 def _get_entity_from_input_table(table: Table):
     if table.source == Table.Source.WORKFLOW_NODE:
@@ -20,8 +25,8 @@ def _get_entity_from_input_table(table: Table):
         return table.integration.source_obj
 
 
-@shared_task
-def run_schedule_for_project(project_id: int):
+@shared_task(bind=True)
+def run_schedule_for_project(self, project_id: int):
 
     project = Project.objects.get(pk=project_id)
 
@@ -56,6 +61,8 @@ def run_schedule_for_project(project_id: int):
 
     ts = TopologicalSorter(graph)
 
+    retry = False
+
     try:
         for entity in ts.static_order():
 
@@ -67,17 +74,22 @@ def run_schedule_for_project(project_id: int):
             for e in graph[entity]:
                 e.refresh_from_db()
 
+            scheduled_parents = [
+                e
+                for e in graph[entity]
+                if (hasattr(e, "is_scheduled") and e.is_scheduled)
+                # connectors are always scheduled
+                or isinstance(e, Connector)
+            ]
+
+            if any(not e.up_to_date for e in scheduled_parents):
+                retry = True
+
             if (
                 hasattr(entity, "is_scheduled")
                 and entity.is_scheduled
                 and not entity.up_to_date
-                and all(
-                    e.up_to_date
-                    for e in graph[entity]
-                    if (hasattr(e, "is_scheduled") and e.is_scheduled)
-                    # connectors are always scheduled
-                    or isinstance(e, Connector)
-                )
+                and all(e.up_to_date for e in scheduled_parents)
             ):
                 entity.run_for_schedule()
 
@@ -86,3 +98,7 @@ def run_schedule_for_project(project_id: int):
         pass
 
     honeybadger_check_in("j6IrRd")
+
+    # We need to keep retrying until the connectors either fail or succeeded
+    if retry:
+        self.retry(countdown=10, max_retries=6 * 6)
