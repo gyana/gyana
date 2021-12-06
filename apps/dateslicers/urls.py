25ee3c267646104fdba699a26d3bad7516f9b371
saschahofmann <sascha.kelevra@gmail.com>
2021-12-06 13:48:48 +0100

    add access control

--- apps/dateslicers/urls.py
+++ apps/dateslicers/urls.py
@@ -1,16 +1,27 @@
+from django.contrib.auth.decorators import login_required
 from django.urls import path
 
 from . import frames
+from .access import dateslicer_of_public, login_and_dateslicer_required
 
 app_name = "dateslicers"
-# TODO: add access control
+
 urlpatterns = [
-    path("new", frames.DateSlicerCreate.as_view(), name="create"),
-    path("<hashid:pk>/update", frames.DateSlicerUpdate.as_view(), name="update"),
+    # Maybe we should move the urls to be under a dashboard url?
+    path("new", login_required(frames.DateSlicerCreate.as_view()), name="create"),
+    path(
+        "<hashid:pk>/update",
+        login_and_dateslicer_required(frames.DateSlicerUpdate.as_view()),
+        name="update",
+    ),
     path(
         "<hashid:pk>/update-public",
-        frames.DateSlicerPublicUpdate.as_view(),
+        dateslicer_of_public(frames.DateSlicerPublicUpdate.as_view()),
         name="update-public",
     ),
-    path("<hashid:pk>/delete", frames.DateSlicerDelete.as_view(), name="delete"),
+    path(
+        "<hashid:pk>/delete",
+        login_and_dateslicer_required(frames.DateSlicerDelete.as_view()),
+        name="delete",
+    ),
 ]
