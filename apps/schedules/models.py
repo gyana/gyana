3425390e7800168222a47ad5462375025550d01b
David Das Kell <davidambrosekell@gmail.com>
2021-12-06 03:55:39 +0000

    remove schedules/schedule.py

--- apps/schedules/models.py
+++ apps/schedules/models.py
@@ -1,8 +1,10 @@
 import json
 from datetime import time, timedelta
 
+import pytz
 from django.db import models
 from django.db.models import Q
+from django.utils import timezone
 from django_celery_beat.models import CrontabSchedule, PeriodicTask
 
 from apps.base.models import BaseModel
@@ -44,9 +46,22 @@ class Schedule(BaseModel):
 
     @property
     def next_daily_sync_in_utc(self):
-        from .schedule import get_next_daily_sync_in_utc_from_schedule
+        # Calculate the next sync time in UTC. It will change over time thanks
+        # to daily savings. Start with the local time of the user, calculate
+        # the next sync time they expect to see, and convert it back to UTC.
 
-        return get_next_daily_sync_in_utc_from_schedule(self)
+        today_local = timezone.now().astimezone(self.project.team.timezone)
+        next_sync_time_local = today_local.replace(
+            hour=self.daily_schedule_time.hour, minute=0, second=0, microsecond=0
+        )
+        if next_sync_time_local < today_local:
+            next_sync_time_local += timedelta(days=1)
+
+        next_sync_time_utc = next_sync_time_local.astimezone(pytz.UTC)
+
+        # for timezones with 15/30/45 minute offset, we prefer to round down
+        # to guarantee it has started
+        return next_sync_time_utc.replace(minute=0)
 
     @property
     def truncated_daily_schedule_time(self):
