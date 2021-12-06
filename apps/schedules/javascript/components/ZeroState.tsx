467097682a66ad9c851f79bd421d322f818582c6
David Das Kell <davidambrosekell@gmail.com>
2021-12-06 00:51:24 +0000

    embolden

--- apps/schedules/javascript/components/ZeroState.tsx
+++ apps/schedules/javascript/components/ZeroState.tsx
@@ -4,7 +4,10 @@ const ZeroState: React.FC = () => (
   <div className='placeholder-scr placeholder-scr--fillscreen gap-10'>
     <div className='flex items-center max-w-lg gap-7'>
       <i className='fas fa-fw fa-calendar text-blue fa-2x'></i>
-      <p>Start adding your integrations, workflows and dashboards to view them here</p>
+      <p>
+        Start adding your <strong>integrations</strong>, <strong>workflows</strong> and{' '}
+        <strong>dashboards</strong> to view them here
+      </p>
     </div>
   </div>
 )
