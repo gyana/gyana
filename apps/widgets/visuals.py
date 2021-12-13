f20356483f70003b6365ee518103f70b5b262a90
saschahofmann <sascha.kelevra@gmail.com>
2021-12-13 10:42:57 +0100

    ugly but works

--- apps/widgets/visuals.py
+++ apps/widgets/visuals.py
@@ -45,10 +45,31 @@ def chart_to_output(widget: Widget, control) -> Dict[str, Any]:
     return {"chart": chart.render()}, chart_id
 
 
+def get_summary_row(query, aggregations, groups):
+    query = query.aggregate(aggregations)
+    summary = clients.bigquery().get_query_results(query.compile()).rows_dict[0]
+    return {**summary, **{group.column: "Total:" for group in groups}}
+
+
 def table_to_output(widget: Widget, control) -> Dict[str, Any]:
     query = pre_filter(widget, control)
 
-    return get_table(query.schema(), query)
+    groups = widget.columns.all()
+    aggregations = [
+        getattr(query[agg.column], agg.function)().name(agg.column)
+        for agg in widget.aggregations.all()
+    ]
+    if widget.show_summary_row:
+        summary = get_summary_row(query, aggregations, groups)
+
+    if groups:
+        query = query.group_by([col.column for col in groups])
+    if aggregations:
+        query = query.aggregate(aggregations)
+
+    return get_table(
+        query.schema(), query, summary if widget.show_summary_row else None
+    )
 
 
 def metric_to_output(widget, control):
