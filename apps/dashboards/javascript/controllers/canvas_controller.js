3a656b17da7f578e12736426ec0ba4ea64da4b73
saschahofmann <sascha.kelevra@gmail.com>
2021-12-17 11:17:16 +0100

    fix positioning on creation

--- apps/dashboards/javascript/controllers/canvas_controller.js
+++ apps/dashboards/javascript/controllers/canvas_controller.js
@@ -11,19 +11,22 @@ export default class extends Controller {
 
     this.element.ondrop = (event) => {
       if (event.dataTransfer.getData('application/gycontrol')) {
-        this.formControlTarget.querySelector('[name=x]').value = event.offsetX - 248
-        this.formControlTarget.querySelector('[name=y]').value = event.offsetY - 195
+        // Default widths is 300
+        this.formControlTarget.querySelector('[name=x]').value = event.offsetX - 150
+        // Default height is 100
+        this.formControlTarget.querySelector('[name=y]').value = event.offsetY - 50
         this.formControlTarget.querySelector('button').disabled = false
         this.formControlTarget.requestSubmit(this.formControlTarget.querySelector('button'))
       } else {
         const data = event.dataTransfer.getData('application/gydashboard')
 
         // Use a hidden form to create a widget and add to canvas via turbo stream
+        console.log(event.offsetX, event.offsetY)
         this.formTarget.querySelector('[name=kind]').value = data
-        // Default widths is 300
-        this.formTarget.querySelector('[name=x]').value = event.offsetX - 150
-        // Default height is 100
-        this.formTarget.querySelector('[name=y]').value = event.offsetY - 50
+        // Default width is double
+        this.formTarget.querySelector('[name=x]').value = event.offsetX - 248
+        // Default height is double
+        this.formTarget.querySelector('[name=y]').value = event.offsetY - 195
         this.formTarget.querySelector('button').disabled = false
 
         this.formTarget.requestSubmit(this.formTarget.querySelector('button'))
