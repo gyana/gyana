33f88e9cdedf3b66a16825499dadaa3587dff6d2
David Das Kell <davidambrosekell@gmail.com>
2021-12-22 02:45:15 +0000

    Out of beta and update pricing logic

---
+++ apps/base/account.py
@@ -0,0 +1,13 @@
+from django.utils.html import mark_safe
+
+
+def is_scheduled_paid_only(is_scheduled_field, project):
+    if project.team.is_free:
+        is_scheduled_field.disabled = True
+        is_scheduled_field.help_text = mark_safe(
+            """Scheduling is only available on a paid plan <a class="link" href="{% url 'teams:plans' team.id %}" data-turbo-frame="_top">learn more</a>"""
+        )
+    else:
+        is_scheduled_field.help_text = (
+            f"Daily at {project.daily_schedule_time} in {project.team.timezone}"
+        )
