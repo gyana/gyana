19717bce767ddb12bd63885797d69690b36e6fe1
David Das Kell <davidambrosekell@gmail.com>
2022-01-07 01:07:18 +0000

    Add a comment on the trick

--- apps/flags/forms.py
+++ apps/flags/forms.py
@@ -20,11 +20,13 @@ class TeamFlagForm(BaseModelForm):
         super().__init__(*args, **kwargs)
 
         flags = self.fields["flags"]
-        qs = Flag.objects.filter(is_public_beta=True).exclude(everyone=True).all()
 
         flags.initial = self.instance.flags.all().values_list("id", flat=True)
-        flags.queryset = qs
-        flags.choices = [(flag.id, flag) for flag in qs]
+        flags.queryset = (
+            Flag.objects.filter(is_public_beta=True).exclude(everyone=True).all()
+        )
+        # a trick to pass through the entire flag model as a label, to render the widget template
+        flags.choices = [(flag.id, flag) for flag in flags.queryset]
 
     def post_save(self, instance):
         instance.flags.set(self.cleaned_data["flags"])
