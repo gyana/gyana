0f5d2873fed70659b702314317b0a7644657a132
David Das Kell <davidambrosekell@gmail.com>
2022-01-15 23:49:04 +0000

    Move website landing page to root

--- apps/website/views.py
+++ apps/website/views.py
@@ -1,5 +1,17 @@
+from django.conf import settings
+from django.shortcuts import redirect
 from django.views.generic import TemplateView
 
 
 class Landing(TemplateView):
     template_name = "website/landing.html"
+
+    def get(self, request, *args, **kwargs):
+
+        if request.user.is_authenticated:
+            return redirect("web:home")
+
+        if not settings.ENABLE_WEBSITE:
+            return redirect("account_login")
+
+        return super().get(request, *args, **kwargs)
