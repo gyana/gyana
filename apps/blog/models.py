191dff4353c683d85b6ec0ff2c9758afed353da2
David Das Kell <davidambrosekell@gmail.com>
2022-01-17 22:24:13 +0000

    poc working

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -1,25 +1,30 @@
+import readtime
 from django.db import models
-from modelcluster.fields import ParentalKey
 from wagtail.admin.edit_handlers import FieldPanel, InlinePanel, MultiFieldPanel
 from wagtail.core.fields import RichTextField
-from wagtail.core.models import Orderable, Page
+from wagtail.core.models import Page
 from wagtail.images.edit_handlers import ImageChooserPanel
 from wagtail.search import index
+from wagtail.snippets.models import register_snippet
 
 
-class BlogIndexPage(Page):
-    intro = RichTextField(blank=True)
+@register_snippet
+class BlogCategory(models.Model):
+    name = models.CharField(max_length=255)
 
-    content_panels = Page.content_panels + [FieldPanel("intro", classname="full")]
+    panels = [FieldPanel("name")]
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "blog categories"
 
 
-class BlogPage(Page):
-
-    # Database fields
-
-    body = RichTextField()
-    date = models.DateField("Post date")
-    feed_image = models.ForeignKey(
+@register_snippet
+class BlogAuthor(models.Model):
+    name = models.CharField(max_length=255)
+    avatar = models.ForeignKey(
         "wagtailimages.Image",
         null=True,
         blank=True,
@@ -27,19 +32,67 @@ class BlogPage(Page):
         related_name="+",
     )
 
-    # Search index configuration
+    panels = [
+        FieldPanel("name"),
+        ImageChooserPanel("avatar"),
+    ]
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "blog authors"
+
+
+class BlogIndexPage(Page):
+    intro = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [FieldPanel("intro", classname="full")]
+
+    def get_context(self, request):
+        context = super().get_context(request)
+        blogpages = self.get_children().live().order_by("-first_published_at")
+        context["blogpages"] = blogpages
+        return context
+
+
+class BlogPage(Page):
+
+    date = models.DateField("Post date")
+    intro = models.CharField(max_length=250)
+    body = RichTextField()
+    feed_image = models.ForeignKey(
+        "wagtailimages.Image",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="+",
+    )
+    category = models.ForeignKey(
+        "blog.BlogCategory",
+        on_delete=models.PROTECT,
+    )
+    author = models.ForeignKey(
+        "blog.BlogAuthor",
+        on_delete=models.PROTECT,
+    )
 
     search_fields = Page.search_fields + [
         index.SearchField("body"),
         index.FilterField("date"),
     ]
 
-    # Editor panels configuration
-
     content_panels = Page.content_panels + [
-        FieldPanel("date"),
+        MultiFieldPanel(
+            [
+                FieldPanel("date"),
+                FieldPanel("category"),
+                FieldPanel("author"),
+            ],
+            heading="Blog information",
+        ),
+        FieldPanel("intro"),
         FieldPanel("body", classname="full"),
-        InlinePanel("related_links", label="Related links"),
     ]
 
     promote_panels = [
@@ -47,19 +100,10 @@ class BlogPage(Page):
         ImageChooserPanel("feed_image"),
     ]
 
-    # Parent page / subpage type rules
-
     # parent_page_types = ["wagtail.Page"]
     # parent_page_types = []
     subpage_types = []
 
-
-class BlogPageRelatedLink(Orderable):
-    page = ParentalKey(BlogPage, on_delete=models.CASCADE, related_name="related_links")
-    name = models.CharField(max_length=255)
-    url = models.URLField()
-
-    panels = [
-        FieldPanel("name"),
-        FieldPanel("url"),
-    ]
+    @property
+    def readtime(self):
+        return readtime.of_html(self.body).text
