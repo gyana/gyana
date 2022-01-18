6b3ec5fd9d24cfe837e5054be53cbb477d3ab315
David Das Kell <davidambrosekell@gmail.com>
2022-01-18 13:51:36 +0000

    migrate tailwind colors, change landing page

--- tailwind.config.js
+++ tailwind.config.js
@@ -28,6 +28,16 @@ module.exports = {
         50: 'var(--c-gray-50)',
         20: 'var(--c-gray-20)',
         10: 'var(--c-gray-10)',
+        // website migration
+        100: '#f3f4f6',
+        200: '#e5e7eb',
+        300: '#d1d5db',
+        400: '#9ca3af',
+        500: '#6b7280',
+        600: '#4b5563',
+        700: '#374151',
+        800: '#1f2937',
+        900: '#111827',
       },
       blue: {
         DEFAULT: 'var(--c-blue)',
@@ -60,6 +70,34 @@ module.exports = {
         10: 'var(--c-red-10)',
       },
       transparent: 'var(--c-transparent)',
+      // website migration
+      // Generated automatically https://tailwind.ink/
+      // We are using indigo as primary color to make it faster to prototype
+      // with Tailwind Components
+      indigo: {
+        50: '#f6fbfd',
+        100: '#e8f7fd',
+        200: '#c5e6fb',
+        300: '#9ecefb',
+        400: '#69a2fb',
+        500: '#3874fa',
+        600: '#2550f5',
+        700: '#203fe0',
+        800: '#1a31af',
+        900: '#152887',
+      },
+      yellow: {
+        50: '#fefce8',
+        100: '#fef9c3',
+        200: '#fef08a',
+        300: '#fde047',
+        400: '#facc15',
+        500: '#eab308',
+        600: '#ca8a04',
+        700: '#a16207',
+        800: '#854d0e',
+        900: '#713f12',
+      },
     },
     extend: {},
     container: {
