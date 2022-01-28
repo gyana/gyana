3aa8d55b315bbba7154173eb240fca0d12880541
David Das Kell <davidambrosekell@gmail.com>
2022-01-28 17:15:12 +0000

    mobile menu

--- apps/web/tests/test_integration.py
+++ apps/web/tests/test_integration.py
@@ -63,6 +63,7 @@ def test_site_links(client):
     assertLink(
         r, "https://gyana-data.typeform.com/to/v2XTy0j3", "Request access", total=4
     )
+    assertLink(r, "https://gyana-data.typeform.com/to/pgpMNnAq", "Talk to us", total=2)
     assertLink(r, "/login/", "Sign in", total=2)
 
     user = CustomUser.objects.create_user("test", email="test@gyana.com")
