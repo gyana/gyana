82e4405650818084f5bbf60e10416c60fbc9d128
David Das Kell <davidambrosekell@gmail.com>
2022-02-01 20:37:33 +0000

    wip stream field with anchor links

--- apps/learn/models.py
+++ apps/learn/models.py
@@ -1,26 +1,42 @@
-import readtime
-from wagtail.admin.edit_handlers import FieldPanel, MultiFieldPanel
-from wagtail.core.fields import RichTextField
+from wagtail.admin.edit_handlers import MultiFieldPanel, StreamFieldPanel
+from wagtail.core import blocks
+from wagtail.core.fields import StreamField
 from wagtail.core.models import Page
+from wagtail.images.blocks import ImageChooserBlock
 from wagtail.search import index
 
 
+class LoomBlock(blocks.URLBlock):
+    class Meta:
+        template = "components/embed_loom.html"
+        icon = "video"
+
+    def get_context(self, value, parent_context=None):
+        context = super().get_context(value, parent_context)
+        context["loom_url"] = value
+        return context
+
+
 class LearnPage(Page):
-    body = RichTextField()
     search_fields = Page.search_fields + [index.SearchField("body")]
 
+    body = StreamField(
+        [
+            ("heading", blocks.CharBlock(form_classname="full title")),
+            ("paragraph", blocks.RichTextBlock()),
+            ("image", ImageChooserBlock()),
+            ("loom", LoomBlock()),
+        ]
+    )
+
     content_panels = Page.content_panels + [
-        FieldPanel("body", classname="full"),
+        StreamFieldPanel("body"),
     ]
 
     promote_panels = [MultiFieldPanel(Page.promote_panels, "Common page configuration")]
 
     parent_page_types = ["wagtailcore.Page", "learn.LearnPage"]
     subpage_types = ["learn.LearnPage"]
-
-    @property
-    def readtime(self):
-        return readtime.of_html(self.body).text
 
     def get_context(self, request):
         context = super().get_context(request)
