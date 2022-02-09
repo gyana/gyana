9f740634c0b02ffd33c7f013f1c4a36be7cd1571
David Das Kell <davidambrosekell@gmail.com>
2022-02-09 17:10:32 +0000

    remove

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,7 +1,6 @@
 from allauth.account.models import EmailAddress
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from django.utils.translation import gettext_lazy as _
 from hijack.contrib.admin import HijackUserAdminMixin
 
 from apps.teams.admin import TeamMembershipInline
