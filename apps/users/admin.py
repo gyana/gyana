0a9b07e4890af848f2b4e97cc64e85675fcc0540
David Das Kell <davidambrosekell@gmail.com>
2022-02-09 15:29:39 +0000

    Add hijack button to user change view

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,6 +1,8 @@
 from allauth.account.models import EmailAddress
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
+from hijack.contrib.admin import HijackUserAdminMixin
 
 from apps.teams.admin import TeamMembershipInline
 
@@ -14,9 +16,10 @@ class EmailAddressInline(admin.TabularInline):
 
 
 @admin.register(CustomUser)
-class CustomUserAdmin(UserAdmin):
+class CustomUserAdmin(HijackUserAdminMixin, UserAdmin):
     list_display = UserAdmin.list_display
     list_per_page = 20
+    change_form_template = "users/admin/change_form.html"
 
     fieldsets = UserAdmin.fieldsets + (
         (
@@ -26,6 +29,12 @@ class CustomUserAdmin(UserAdmin):
     )
 
     inlines = [TeamMembershipInline, EmailAddressInline]
+
+    def change_view(self, request, object_id, form_url="", extra_context=None):
+        extra_context = {'hijack_button': self.hijack_button(
+            request, CustomUser.objects.get(pk=object_id)
+        )}
+        return super().change_view(request, object_id, form_url, extra_context)
 
 
 @admin.register(ApprovedWaitlistEmail)
