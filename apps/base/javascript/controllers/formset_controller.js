0bb9a292f26c564acc6b32e59eb82ba1017253cf
David Das Kell <davidambrosekell@gmail.com>
2022-03-03 16:00:56 +0000

    working implementation

--- apps/base/javascript/controllers/formset_controller.js
+++ apps/base/javascript/controllers/formset_controller.js
@@ -2,6 +2,35 @@ import { Controller } from '@hotwired/stimulus'
 import { GyanaEvents } from 'apps/base/javascript/events'
 import Sortable from 'sortablejs'
 
+/*
+Get the new sort index for each element, based on the insertion index.
+
+Elements are sorted by big integers, in reverse numeric order, with the final element
+at "0". This enables the backend to add new elements without knowing about
+the other elements.
+*/
+
+const getSortOrder = (sortOrder, idx) => {
+  if (idx === 0) {
+    // prepend to start of array with +1n
+    return [sortOrder[1] + 1n, ...sortOrder.slice(1)]
+  } else if (idx === sortOrder.length - 1) {
+    // append with 0n, shift everything else back by +1n
+    return [...sortOrder.slice(0, -1).map((idx) => idx + 1n), 0n]
+  } else {
+    // insert, doubling all indexes if required
+    let newSortOrder = [...sortOrder]
+
+    if (newSortOrder[idx - 1] - newSortOrder[idx + 1] === 1n) {
+      newSortOrder = newSortOrder.map((index) => index * 2n)
+    }
+
+    newSortOrder[idx] = newSortOrder[idx - 1] - 1n
+
+    return newSortOrder
+  }
+}
+
 // Dynamically add and remove formset in Django
 // Inspired by https://github.com/stimulus-components/stimulus-rails-nested-form
 
@@ -15,7 +44,20 @@ export default class extends Controller {
   connect() {
     this.wrapperSelector = this.wrapperSelectorValue || '.formset-wrapper'
 
-    this.sortable = Sortable.create(this.targetTarget)
+    this.sortable = Sortable.create(this.targetTarget, {
+      onEnd: (event) => {
+        const sortIndexInputs = Array.from(
+          this.element.querySelectorAll('input[name*=sort_index]')
+        )
+
+        const sortOrder = sortIndexInputs.map((el) => BigInt(el.value))
+        const newSortOrder = getSortOrder(sortOrder, event.newIndex)
+
+        for (const [idx, el] of sortIndexInputs.entries()) {
+          el.value = newSortOrder[idx]
+        }
+      },
+    })
   }
 
   add(e) {
@@ -26,6 +68,10 @@ export default class extends Controller {
     )
     const total = parseInt(TOTAL_FORMS.value)
     TOTAL_FORMS.value = parseInt(total) + 1
+
+    for (const el of this.element.querySelectorAll('input[name*=sort_index]')) {
+      el.value = (BigInt(el.value) + 1n).toString()
+    }
 
     window.dispatchEvent(new CustomEvent(GyanaEvents.UPDATE_FORM_COUNT))
     this.dispatch('add', { event: e })
--- apps/columns/models.py
+++ apps/columns/models.py
@@ -191,6 +191,9 @@ class AggregationColumn(ColumnSettings, SaveParentModel):
 
 
 class SortColumn(SaveParentModel):
+    class Meta:
+        ordering = ("-sort_index",)
+
     node = models.ForeignKey(
         Node, on_delete=models.CASCADE, related_name="sort_columns"
     )
@@ -198,6 +201,9 @@ class SortColumn(SaveParentModel):
     column = models.CharField(
         max_length=settings.BIGQUERY_COLUMN_NAME_LENGTH,
     )
+    # formset sort index, elements sorted in reverse order with the final element at "0"
+    # text field for arbitrary length integer, parsed at BigInt in javascript
+    sort_index = models.TextField(default="0")
 
 
 class EditColumn(AbstractOperationColumn):
