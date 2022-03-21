7775ba043094f030c460388a4d96130a81585599
David Das Kell <davidambrosekell@gmail.com>
2022-03-21 17:08:58 +0000

    Fix teams:plans -> teams:pricing links

--- apps/teams/views.py
+++ apps/teams/views.py
@@ -36,7 +36,7 @@ class TeamCreate(TurboCreateView):
         return kwargs
 
     def get_success_url(self) -> str:
-        return reverse("teams:plans", args=(self.object.id,))
+        return reverse("teams:detail", args=(self.object.id,))
 
 
 class TeamPlans(TurboUpdateView):
