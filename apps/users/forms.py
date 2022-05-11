53ae1746543b6654c91cdfe7c3e61420f75ea41c
David Kell <davidambrosekell@gmail.com>
2022-05-11 09:59:35 +0100

    Redundant code

--- apps/users/forms.py
+++ apps/users/forms.py
@@ -65,9 +65,6 @@ class UserLoginForm(LoginForm):
         del self.fields["login"].widget.attrs["placeholder"]
         del self.fields["password"].widget.attrs["placeholder"]
 
-    def login(self, *args, **kwargs):
-        return super(UserLoginForm, self).login(*args, **kwargs)
-
 
 class CustomUserChangeForm(UserChangeForm):
     email = forms.EmailField(required=True, label="Email Address")
