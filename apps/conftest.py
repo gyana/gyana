d3dacdcab62c311f45e498dba512d247e694f9f3
saschahofmann <sascha.kelevra@gmail.com>
2022-05-24 16:54:37 +0200

    override pytest setting

--- apps/conftest.py
+++ apps/conftest.py
@@ -1,3 +1,10 @@
+import pytest
+
 from apps.base.tests.client import *  # noqa
 from apps.base.tests.factory import *  # noqa
 from apps.base.tests.fixtures import *  # noqa
+
+
+@pytest.fixture(autouse=True)
+def override_settings(settings):
+    settings.CACHEOPS_ENABLED = False
