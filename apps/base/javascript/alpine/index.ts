020839cf7b9e0844216a35290cc28d107ce8981d
David Kell <davidambrosekell@gmail.com>
2023-10-07 15:56:11 +0100

    Remove base javascript folder

--- apps/base/javascript/alpine/index.ts
+++
@@ -1,7 +0,0 @@
-import tooltip from './tooltip'
-import popover from './popover'
-
-document.addEventListener('alpine:init', () => {
-  Alpine.directive('tooltip', tooltip)
-  Alpine.data('popover', popover)
-})
