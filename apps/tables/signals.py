1816b2aef527b5b532a7c98515141ee1201eaf7e
saschahofmann <sascha.kelevra@gmail.com>
2023-12-12 21:04:12 +0100

    Make delete backend table backend agnostic

--- apps/tables/signals.py
+++ apps/tables/signals.py
@@ -2,18 +2,18 @@ from django.conf import settings
 from django.db.models.signals import post_delete
 from django.dispatch.dispatcher import receiver
 
-from apps.base.engine import bigquery as bq
+from apps.base.engine import get_backend_client
 
 from .models import Table
 
 
 @receiver(post_delete, sender=Table)
-def delete_bigquery_table(sender, instance, *args, **kwargs):
+def delete_backend_table(sender, instance, *args, **kwargs):
     if settings.MOCK_REMOTE_OBJECT_DELETION:
         return
 
     # hotfix for cloned templates where bq dataset name is invalid
     if " copy " in instance.bq_id:
         return
-
-    bq.bigquery().delete_table(instance.bq_id, not_found_ok=True)
+    # TODO: Make sure it works for postgres backend
+    get_backend_client().client.drop_table(instance.bq_id, False=True)
