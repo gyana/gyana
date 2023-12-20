64d8c7f27fa58fdeed94ff61fcaabdca47a7b622
saschahofmann <sascha.kelevra@gmail.com>
2023-12-20 19:02:55 +0100

    Forgot to add export_to_csv to bigquery client

--- apps/base/engine/bigquery.py
+++ apps/base/engine/bigquery.py
@@ -273,3 +273,13 @@ class BigQueryClient(BaseClient):
                     for field in header_values
                 ],
             )
+
+    def export_to_csv(self, query, gcs_path):
+        client = bigquery()
+        # Create temporary table in bigquery
+        job = client.query(query.compile())
+        job.result()
+
+        # Use temporary table and export to GCS
+        extract_job = client.extract_table(job.destination, gcs_path)
+        extract_job.result()
