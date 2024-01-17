01f4aeb15cc5e45a9681061ed6e1c25dd2c3779f
David Kell <davidambrosekell@gmail.com>
2024-01-17 12:06:26 +0000

    Remove Alpine tricks

--- apps/base/javascript/alpine/index.ts
+++ apps/base/javascript/alpine/index.ts
@@ -1,18 +1,7 @@
 import tooltip from './tooltip'
 import popover from './popover'
 
-const formset = (el, { Alpine }) => {
-  return Array.from(el.querySelectorAll('.formset-wrapper')).map((child) => child._x_dataStack).filter(child => child !== undefined).map(child => Alpine.mergeProxies(child))
-}
-
 document.addEventListener('alpine:init', () => {
   Alpine.directive('tooltip', tooltip)
   Alpine.data('popover', popover)
-  // https://github.com/ryangjchandler/alpine-parent
-  Alpine.magic('parent', (el, { Alpine }) => {
-      return Alpine.mergeProxies(
-          Alpine.closestDataStack(el).slice(1),
-      )
-  })
-  Alpine.magic('formset', formset)
 })
