6d881ef6f65bab0ceae1d823feaf6c2f44755aed
David Kell <davidambrosekell@gmail.com>
2024-02-27 20:17:11 +0000

    test workflows

--- apps/base/tests/e2e/test_workflows.py
+++ apps/base/tests/e2e/test_workflows.py
@@ -3,72 +3,10 @@ from playwright.sync_api import expect
 
 pytestmark = pytest.mark.django_db(transaction=True)
 
-# // https://github.com/wbkd/react-flow/blob/main/cypress/support/commands.js
-# Cypress.Commands.add('reactFlowDrag', (objId, { x, y }) => {
-#   return cy
-#     .get(`[data-id=${objId}]`)
-#     .trigger('mousedown', { which: 1 })
-#     .trigger('mousemove', { clientX: x, clientY: y })
-#     .trigger('mouseup', { force: true })
-# })
 
-
-def react_flow_drag(page, obj_id, x, y):
-    # Select the element
-    element = page.query_selector(f'[data-id="{obj_id}"]')
-
-    # Dispatch mousedown event
-    page.evaluate(
-        f"(element) => {{ element.dispatchEvent(new MouseEvent('mousedown', {{ button: 0 }})); }}",
-        element,
-    )
-
-    # Dispatch mousemove event
-    page.mouse.move(x, y)
-
-    # Dispatch mouseup event
-    page.evaluate(
-        f"(element) => {{ element.dispatchEvent(new MouseEvent('mouseup', {{ button: 0 }})); }}",
-        element,
-    )
-
-
-def connect_nodes(page, source, target):
-    # Select the source element
-    source_element = page.query_selector(
-        f'[data-id="{source}"] .react-flow__handle.source'
-    )
-
-    # Dispatch mousedown event on source element
-    page.evaluate(
-        f"(element) => {{ element.dispatchEvent(new MouseEvent('mousedown', {{ button: 0 }})); }}",
-        source_element,
-    )
-
-    # Select the target element
-    target_element = page.query_selector(
-        f'[data-id="{target}"] .react-flow__handle.target'
-    )
-
-    # Create a new DataTransfer object
-    # data_transfer = page.evaluate("() => new DataTransfer()")
-
-    # Dispatch mousemove event on target element with DataTransfer object
-    page.evaluate(
-        f"(element, dataTransfer) => {{ element.dispatchEvent(new MouseEvent('mousemove', {{ dataTransfer: new DataTransfer() }})); }}",
-        target_element,
-        # data_transfer,
-    )
-
-    # Dispatch mouseup event on target element with DataTransfer object
-    page.evaluate(
-        f"(element, dataTransfer) => {{ element.dispatchEvent(new MouseEvent('mouseup', {{ force: true, dataTransfer: new DataTransfer() }})); }}",
-        target_element,
-        # data_transfer,
-    )
-
-
-def test_workflow_editor(page, live_server, project, integration_table_factory):
+def test_workflow_editor(
+    page, live_server, project, integration_table_factory, celery_worker
+):
     table = integration_table_factory(
         project=project,
         name="upload_000000001",
@@ -84,17 +22,53 @@ def test_workflow_editor(page, live_server, project, integration_table_factory):
     page.locator('input[id="name"]').fill("Magical workflow")
 
     # drop and configure an input node
-    page.locator("#dnd-node-input").drag_to(page.locator(".react-flow"))
+    page.locator("#dnd-node-input").drag_to(
+        page.locator(".react-flow"),
+        target_position={"x": 150, "y": 300},
+    )
 
     page.locator('[data-id="1"]').dblclick()
     page.get_by_text("store_info").click()
     page.get_by_text("Employees").wait_for()
     assert page.get_by_text("Blackpool").count() == 4
     page.locator(".modal__close").click()
-    react_flow_drag(page, 1, 150, 300)
 
     # drop, connect and configure select node
-    page.locator("#dnd-node-select").drag_to(page.locator(".react-flow"))
-    connect_nodes(page, 1, 2)
+    page.locator("#dnd-node-select").drag_to(
+        page.locator(".react-flow"), target_position={"x": 300, "y": 100}
+    )
+    page.locator('[data-id="2"]').wait_for()
+    page.locator('[data-id="1"] .react-flow__handle.source').drag_to(
+        page.locator('[data-id="2"] .react-flow__handle.target')
+    )
 
-    page.pause()
+    page.locator('[data-id="2"]').dblclick()
+    page.get_by_text("Location").click()
+    page.get_by_text("Employees").click()
+    page.get_by_text("Owner").click()
+
+    expect(page.get_by_text("Save & Close")).not_to_be_disabled()
+    page.get_by_text("Save & Close").click()
+
+    # drop, connect and name output node
+    page.locator("#dnd-node-output").drag_to(
+        page.locator(".react-flow"), target_position={"x": 450, "y": 300}
+    )
+
+    page.locator('[data-id="3"]').wait_for()
+    page.locator('[data-id="2"] .react-flow__handle.source').drag_to(
+        page.locator('[data-id="3"] .react-flow__handle.target')
+    )
+
+    page.locator('[data-id="3"]').dblclick()
+    page.locator('#node-update-form input[name="name"]').fill("Goblet of fire")
+    page.get_by_text("Save & Close").click()
+
+    # run workflow
+    page.locator('[data-cy="workflow-run"]').click()
+    page.get_by_text("Last successful run").wait_for()
+    page.locator(".fa-sitemap").click()
+
+    page.get_by_text("Magical workflow").wait_for()
+    page.locator(".fa-check-circle").hover()
+    page.get_by_text("Workflow ran successfully and is up to date").wait_for()
