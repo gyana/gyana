dedd23f0a6fbd588b64e30407c1501aea639751c
David Kell <davidambrosekell@gmail.com>
2024-03-04 22:47:59 +0000

    Reset sequences between e2e tests

--- apps/base/tests/e2e/conftest.py
+++ apps/base/tests/e2e/conftest.py
@@ -1,4 +1,5 @@
 import pytest
+from django.db import connection
 
 
 @pytest.fixture(autouse=True)
@@ -24,3 +25,20 @@ def bigquery():
 @pytest.fixture(scope="session")
 def celery_config():
     return {"broker_url": "redis://localhost:6379/1", "result_backend": "django-db"}
+
+
+@pytest.fixture(autouse=True)
+def reset_sequences():
+    yield
+    with connection.cursor() as cursor:
+        cursor.execute(
+            """DO $$
+DECLARE
+    row record;
+BEGIN
+    FOR row IN SELECT sequence_name FROM information_schema.sequences WHERE sequence_schema = 'public'
+    LOOP
+        EXECUTE 'ALTER SEQUENCE ' || quote_ident(row.sequence_name) || ' RESTART WITH 1';
+    END LOOP;
+END $$;"""
+        )
