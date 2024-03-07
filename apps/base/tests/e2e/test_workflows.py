890be8b6d21df8a22440091c3dac2f9703018cc0
David Kell <davidambrosekell@gmail.com>
2024-03-07 14:08:24 +0000

    finish workflow tests

--- apps/base/tests/e2e/test_workflows.py
+++ apps/base/tests/e2e/test_workflows.py
@@ -1,5 +1,9 @@
+import re
+
 import pytest
 from playwright.sync_api import expect
+
+from .conftest import BIGQUERY_TIMEOUT
 
 pytestmark = pytest.mark.django_db(transaction=True)
 
@@ -72,3 +76,82 @@ def test_workflow_editor(
     page.get_by_text("Magical workflow").wait_for()
     page.locator(".fa-check-circle").hover()
     page.get_by_text("Workflow ran successfully and is up to date").wait_for()
+
+
+def test_schema_error(page, live_server, project):
+    page.force_login(live_server)
+    page.goto(live_server.url + "/projects/1/workflows")
+
+    page.evaluate("() => delete window.schema")
+
+    page.locator('button[type="submit"]').first.click()
+
+    page.get_by_text("Loading").wait_for()
+    page.get_by_text("Something went wrong!").wait_for(timeout=BIGQUERY_TIMEOUT)
+
+
+def test_nodes_loading_error(page, live_server, project):
+    page.force_login(live_server)
+    page.goto(live_server.url + "/projects/1/workflows")
+
+    page.route(
+        live_server.url + "/nodes/api/nodes/?workflow=1",
+        lambda route: route.fulfill(status=500),
+    )
+
+    page.locator('button[type="submit"]').first.click()
+
+    page.get_by_text("Failed loading your nodes!").wait_for(timeout=BIGQUERY_TIMEOUT)
+
+
+def test_workflow_format(page, live_server, project):
+    page.force_login(live_server)
+    page.goto(live_server.url + "/projects/1/workflows")
+
+    page.get_by_text("Create a new workflow").click()
+
+    page.locator("#dnd-node-input").drag_to(
+        page.locator(".react-flow"), target_position={"x": 450, "y": 400}
+    )
+
+    # drop, connect and configure select node
+    page.locator("#dnd-node-select").drag_to(
+        page.locator(".react-flow"), target_position={"x": 300, "y": 100}
+    )
+    page.locator('[data-id="2"]').wait_for()
+    page.locator('[data-id="1"] .react-flow__handle.source').drag_to(
+        page.locator('[data-id="2"] .react-flow__handle.target')
+    )
+
+    # drop, connect and name output node
+    page.locator("#dnd-node-output").drag_to(
+        page.locator(".react-flow"), target_position={"x": 100, "y": 300}
+    )
+
+    page.locator('[data-id="3"]').wait_for()
+    page.locator('[data-id="2"] .react-flow__handle.source').drag_to(
+        page.locator('[data-id="3"] .react-flow__handle.target')
+    )
+
+    pattern = r"transform: translate\((-?\d*\.?\d+)px, (-?\d*\.?\d+)px\);"
+
+    positions = {
+        x.get_attribute("data-id"): float(
+            re.search(pattern, x.get_attribute("style")).groups()[0]
+        )
+        for x in page.locator("[data-id]").all()
+    }
+
+    ids = [k for k, _ in sorted(positions.items(), key=lambda item: item[1])]
+    assert ids == ["3", "2", "1"]
+
+    page.locator(".react-flow__controls-button").nth(4).click()
+
+    positions = {
+        x.get_attribute("data-id"): float(
+            re.search(pattern, x.get_attribute("style")).groups()[0]
+        )
+        for x in page.locator("[data-id]").all()
+    }
+    ids = [k for k, _ in sorted(positions.items(), key=lambda item: item[1])]
+    assert ids == ["1", "2", "3"]
