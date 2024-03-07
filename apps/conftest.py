b20533dacf3236107cf77ced9fe40c370d0cc350
David Kell <davidambrosekell@gmail.com>
2024-03-07 10:52:09 +0000

    Remove conftest plugins

--- apps/conftest.py
+++ apps/conftest.py
@@ -4,8 +4,6 @@ from apps.base.tests.client import *  # noqa
 from apps.base.tests.factory import *  # noqa
 from apps.base.tests.fixtures import *  # noqa
 
-pytest_plugins = ("celery.contrib.pytest",)
-
 
 def pytest_configure():
     # support for playwright https://github.com/microsoft/playwright-python/issues/224
