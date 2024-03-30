9153f40b4812ae122c0040d5586aa678e87f9bca
David Kell <davidambrosekell@gmail.com>
2024-03-30 18:13:48 +0000

    Fix e2e tests

--- apps/base/tests/e2e/test_uploads.py
+++ apps/base/tests/e2e/test_uploads.py
@@ -38,58 +38,20 @@ def test_upload_valid_csv(page, live_server, project, celery_worker, bigquery):
     expect(page.get_by_text("Sync")).not_to_be_attached()
 
 
-def test_upload_streamed_with_chunks(
-    page, live_server, project, celery_worker, bigquery, upload_factory
-):
-    # prevent upload from overriding upload for previous test (used in workflows/dashboards)
-    upload_factory(integration__project__team=project.team)
-
-    page.force_login(live_server)
-    page.goto(live_server.url + "/projects/1/integrations")
-
-    # minimum chunk size allowed "a multiple of 256 KiB (256 x 1024 bytes)"
-    # https://cloud.google.com/storage/docs/performing-resumable-uploads#chunked-upload
-    # by construction file is 1.1 MB = 2 chunks
-    page.evaluate("() => window.__cypressChunkSize__ = 512 * 1024;")
-
-    page.get_by_text("Upload CSV").click()
-
-    page.locator("input[type=file]").set_input_files(f"{fixtures}/fifa.csv")
-
-    page.get_by_text("Confirm", exact=True).click(timeout=BIGQUERY_TIMEOUT)
-    page.get_by_text("Preview", exact=True).wait_for()
-    # 2250 lines of CSV including header
-    page.get_by_text("2,249").wait_for()
-
-
 def test_upload_failures(page, live_server, project):
     page.force_login(live_server)
     page.goto(live_server.url + "/projects/1/integrations/uploads/new")
 
     # invalid format - better way to test this?
-    assert page.locator("input[type=file]").get_attribute("accept") == ".csv"
+    assert (
+        page.locator("input[type=file]").get_attribute("accept")
+        == ".csv,.tsv,text/csv,text/plain"
+    )
 
     # file is too large
     page.goto(live_server.url + "/projects/1/integrations")
-    page.evaluate("() => window.__cypressMaxSize__ = 128;")
+    page.evaluate("() => window.__cypressMaxSize__ = 1;")
     page.get_by_text("Upload CSV").click()
 
-    page.locator("input[type=file]").set_input_files(f"{fixtures}/store_info.csv")
-    page.get_by_text("Errors occurred when uploading your file").wait_for()
-    page.get_by_text("This file is too large").wait_for()
-
-    # upload errors e.g. bad connectivity or Google is down
-    page.goto(live_server.url + "/projects/1/integrations")
-    page.evaluate("() => window.__cypressMaxBackoff__ = 1;")
-    page.get_by_text("Upload CSV").click()
-
-    def handle_put(route):
-        if route.request.method == "PUT":
-            return route.fulfill(status=500)
-        return route.continue_()
-
-    page.route("https://storage.googleapis.com/gyana-local/**/*", handle_put)
-
-    page.locator("input[type=file]").set_input_files(f"{fixtures}/store_info.csv")
-    page.get_by_text("Errors occurred when uploading your file").wait_for()
-    page.get_by_text("Server error, try again later").wait_for()
+    page.locator("input[type=file]").set_input_files(f"{fixtures}/fifa.csv")
+    page.get_by_text("File is too big (1.08MiB). Max filesize: 1MiB.").wait_for()
