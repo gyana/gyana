0e56c8ee67e8323d577aeaecd93a4f2a38fa9b11
David Kell <davidambrosekell@gmail.com>
2024-04-01 15:23:18 +0100

    Fix tests

--- apps/base/tests/e2e/test_uploads.py
+++ apps/base/tests/e2e/test_uploads.py
@@ -38,6 +38,28 @@ def test_upload_valid_csv(page, live_server, project, celery_worker):
     expect(page.get_by_text("Sync")).not_to_be_attached()
 
 
+def test_upload_streamed_with_chunks(
+    page, live_server, project, celery_worker, upload_factory
+):
+    # prevent upload from overriding upload for previous test (used in workflows/dashboards)
+    upload_factory(integration__project__team=project.team)
+
+    page.force_login(live_server)
+    page.goto(live_server.url + "/projects/1/integrations")
+
+    # by construction file is 1.1 MB = 2 chunks
+    page.evaluate("() => window.__cypressChunkSize__ = 512 * 1024;")
+
+    page.get_by_text("Upload CSV").click()
+
+    page.locator("input[type=file]").set_input_files(f"{fixtures}/fifa.csv")
+
+    page.get_by_text("Confirm", exact=True).click(timeout=BIGQUERY_TIMEOUT)
+    page.get_by_text("Preview", exact=True).wait_for()
+    # 2250 lines of CSV including header
+    page.get_by_text("2,249").wait_for()
+
+
 def test_upload_failures(page, live_server, project):
     page.force_login(live_server)
     page.goto(live_server.url + "/projects/1/integrations/uploads/new")
@@ -54,4 +76,14 @@ def test_upload_failures(page, live_server, project):
     page.get_by_text("Upload CSV").click()
 
     page.locator("input[type=file]").set_input_files(f"{fixtures}/fifa.csv")
-    page.get_by_text("File is too big (1.08MiB). Max filesize: 1MiB.").wait_for()
+    page.get_by_text("File is too large fifa.csv Maximum file size is 1 KB").wait_for()
+
+    # upload errors e.g. bad connectivity or Google is down
+    page.goto(live_server.url + "/projects/1/integrations")
+    page.evaluate("() => window.__cypressMaxBackoff__ = 1;")
+    page.get_by_text("Upload CSV").click()
+
+    page.route("**/*", lambda r: r.fulfill(status=500))
+
+    page.locator("input[type=file]").set_input_files(f"{fixtures}/store_info.csv")
+    page.get_by_text("Error during upload store_info.csv tap to retry").wait_for()
